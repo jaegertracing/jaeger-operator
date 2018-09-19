61d8835c5a7aea996ac2fc366b2d389a08b3f128
Juraci Paixão Kröhling <juraci@kroehling.de>
2018-09-19 11:50:18 +0200

    Add release scripts
    This commit adds support for releasing the operator. It adds a couple of scripts that are supposed to be executed by travis. The scripts build and push docker images, as well as change the 'deploy/operator.yaml' to use this latest release tag. Once that is done, the change to the operator.yaml is committed, a release is tagged and pushed to GitHub.
    
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# echo "Installing kubectl"
-# curl -sLo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.10.0/bin/linux/amd64/kubectl > /dev/null
-# chmod +x kubectl
-# sudo mv kubectl /usr/local/bin/
-
 echo "Installing gosec"
 go get github.com/securego/gosec/cmd/gosec/...
 
 echo "Installing golint"
 go get -u golang.org/x/lint/golint
-
-# if [ ! -d ${HOME}/google-cloud-sdk ]; then
-#     curl https://sdk.cloud.google.com | bash;
-# fi
-
-# gcloud auth activate-service-account --key-file test/client-secret.json
-# gcloud container clusters get-credentials jpkroehling-jaeger-operator-master --zone us-central1-a --project jpkroehling-jaeger-operator
