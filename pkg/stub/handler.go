e418edbe8df91134e74503da3a2adbd9a6808333
Juraci Paixão Kröhling <juraci@kroehling.de>
2018-10-09 15:55:54 +0200

    Normalize options on the stub and update the normalized CR
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -2,6 +2,7 @@ package stub
 
 import (
 	"context"
+	"strings"
 
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
 	"github.com/sirupsen/logrus"
@@ -26,16 +27,56 @@ func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 	case *v1alpha1.Jaeger:
 		if event.Deleted {
 			logrus.Infof("Deleting '%s'", o.Name)
-			// aparently, everything created by the CR should cascade the deletion to the
-			// resources it created, so, no need to do anything
-			// on a next version, we could think about cleaning up the data pertaining to
-			// us at the storage level, but not for now
 			return nil
+		}
+
+		// we need a name!
+		if o.Name == "" {
+			logrus.Infof("This Jaeger instance was created without a name. Setting it to 'my-jaeger'")
+			o.Name = "my-jaeger"
+		}
+
+		// normalize the storage type
+		if o.Spec.Storage.Type == "" {
+			logrus.Infof("Storage type wasn't provided for the Jaeger instance '%v'. Falling back to 'memory'", o.Name)
+			o.Spec.Storage.Type = "memory"
+		}
+
+		if unknownStorage(o.Spec.Storage.Type) {
+			logrus.Infof(
+				"The provided storage type for the Jaeger instance '%v' is unknown ('%v'). Falling back to 'memory'",
+				o.Name,
+				o.Spec.Storage.Type,
+			)
+			o.Spec.Storage.Type = "memory"
+		}
+
+		// normalize the deployment strategy
+		if strings.ToLower(o.Spec.Strategy) != "production" {
+			o.Spec.Strategy = "all-in-one"
+		}
+
+		// check for incompatible options
+		// if the storage is `memory`, then the only possible strategy is `all-in-one`
+		if strings.ToLower(o.Spec.Storage.Type) == "memory" && o.Spec.Strategy != "all-in-one" {
+			logrus.Warnf(
+				"No suitable storage was provided for the Jaeger instance '%v'. Falling back to all-in-one. Storage type: '%v'",
+				o.Name,
+				o.Spec.Storage.Type,
+			)
+			o.Spec.Strategy = "all-in-one"
+		}
+
+		// we store back the "fixed" CR, so that what is stored reflects what is being used
+		if err := sdk.Update(o); err != nil {
+			logrus.Errorf("failed to update %v", o)
+			return err
 		}
 
 		ctrl := controller.NewController(ctx, o)
 
 		objs := ctrl.Create()
+		created := false
 		for _, obj := range objs {
 			err := sdk.Create(obj)
 			if err != nil && !apierrors.IsAlreadyExists(err) {
@@ -44,8 +85,12 @@ func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 			}
 
 			if err == nil {
-				logrus.Infof("Created '%v'", o.Name)
+				created = true
 			}
+		}
+
+		if created {
+			logrus.Infof("Configured %v", o.Name)
 		}
 
 		objs = ctrl.Update()
@@ -58,4 +103,21 @@ func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 		}
 	}
 	return nil
+}
+
+func unknownStorage(typ string) bool {
+	known := []string{
+		"memory",
+		"kafka",
+		"elasticsearch",
+		"cassandra",
+	}
+
+	for _, k := range known {
+		if strings.ToLower(typ) == k {
+			return false
+		}
+	}
+
+	return true
 }
