0de1241eabcdf6471f5c982cb09f401bd69daecc
Chun Lin Yang <clyang@cn.ibm.com>
2018-11-08 13:04:10 +0800

    Add more unit cases for util
    Signed-off-by: Chun Lin Yang <clyang@cn.ibm.com>

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -10,20 +10,21 @@ func TestRemoveDuplicatedVolumes(t *testing.T) {
 	volumes := []v1.Volume{
 		v1.Volume{
 			Name:         "volume1",
-			VolumeSource: v1.VolumeSource{},
+			VolumeSource: v1.VolumeSource{HostPath: &v1.HostPathVolumeSource{Path: "/data1"}},
 		},
 		v1.Volume{
 			Name:         "volume2",
-			VolumeSource: v1.VolumeSource{},
+			VolumeSource: v1.VolumeSource{HostPath: &v1.HostPathVolumeSource{Path: "/data2"}},
 		},
 		v1.Volume{
 			Name:         "volume1",
-			VolumeSource: v1.VolumeSource{},
+			VolumeSource: v1.VolumeSource{HostPath: &v1.HostPathVolumeSource{Path: "/data3"}},
 		},
 	}
 
 	assert.Len(t, RemoveDuplicatedVolumes(volumes), 2)
 	assert.Equal(t, "volume1", volumes[0].Name)
+	assert.Equal(t, "/data1", volumes[0].VolumeSource.HostPath.Path)
 	assert.Equal(t, "volume2", volumes[1].Name)
 }
 
@@ -39,11 +40,12 @@ func TestRemoveDuplicatedVolumeMounts(t *testing.T) {
 		},
 		v1.VolumeMount{
 			Name:     "data1",
-			ReadOnly: false,
+			ReadOnly: true,
 		},
 	}
 
 	assert.Len(t, RemoveDuplicatedVolumeMounts(volumeMounts), 2)
 	assert.Equal(t, "data1", volumeMounts[0].Name)
+	assert.Equal(t, false, volumeMounts[0].ReadOnly)
 	assert.Equal(t, "data2", volumeMounts[1].Name)
 }
