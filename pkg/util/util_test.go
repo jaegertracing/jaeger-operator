5a7a6ad953945b54bd273ec05452dfebe1f3733a
Gary Brown <gary@brownuk.com>
2018-11-09 14:27:51 +0000

    Fix test coverage
    Signed-off-by: Gary Brown <gary@brownuk.com>

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -72,3 +72,61 @@ func TestMergeAnnotations(t *testing.T) {
 	assert.Equal(t, "world", merged.Annotations["hello"])
 	assert.Equal(t, "false", merged.Annotations["prometheus.io/scrape"])
 }
+
+func TestMergeMountVolumes(t *testing.T) {
+	generalSpec := v1alpha1.JaegerCommonSpec{
+		VolumeMounts: []v1.VolumeMount{
+			v1.VolumeMount{
+				Name:     "data1",
+				ReadOnly: true,
+			},
+		},
+	}
+	specificSpec := v1alpha1.JaegerCommonSpec{
+		VolumeMounts: []v1.VolumeMount{
+			v1.VolumeMount{
+				Name:     "data1",
+				ReadOnly: false,
+			},
+			v1.VolumeMount{
+				Name:     "data2",
+				ReadOnly: false,
+			},
+		},
+	}
+
+	merged := Merge([]v1alpha1.JaegerCommonSpec{specificSpec, generalSpec})
+
+	assert.Equal(t, "data1", merged.VolumeMounts[0].Name)
+	assert.Equal(t, false, merged.VolumeMounts[0].ReadOnly)
+	assert.Equal(t, "data2", merged.VolumeMounts[1].Name)
+}
+
+func TestMergeVolumes(t *testing.T) {
+	generalSpec := v1alpha1.JaegerCommonSpec{
+		Volumes: []v1.Volume{
+			v1.Volume{
+				Name:         "volume1",
+				VolumeSource: v1.VolumeSource{HostPath: &v1.HostPathVolumeSource{Path: "/data3"}},
+			},
+		},
+	}
+	specificSpec := v1alpha1.JaegerCommonSpec{
+		Volumes: []v1.Volume{
+			v1.Volume{
+				Name:         "volume1",
+				VolumeSource: v1.VolumeSource{HostPath: &v1.HostPathVolumeSource{Path: "/data1"}},
+			},
+			v1.Volume{
+				Name:         "volume2",
+				VolumeSource: v1.VolumeSource{HostPath: &v1.HostPathVolumeSource{Path: "/data2"}},
+			},
+		},
+	}
+
+	merged := Merge([]v1alpha1.JaegerCommonSpec{specificSpec, generalSpec})
+
+	assert.Equal(t, "volume1", merged.Volumes[0].Name)
+	assert.Equal(t, "/data1", merged.Volumes[0].VolumeSource.HostPath.Path)
+	assert.Equal(t, "volume2", merged.Volumes[1].Name)
+}
