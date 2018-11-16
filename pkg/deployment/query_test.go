ad3d290acfd6655ff21e5076ea5714b6ff123eeb
Annanay <annanay.a@media.net>
2018-11-16 18:19:47 +0530

    Add relavant test for jaeger-query
    Signed-off-by: Annanay <annanay.a@media.net>

--- pkg/deployment/query_test.go
+++ pkg/deployment/query_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/jaegertracing/jaeger-operator/pkg/apis/io/v1alpha1"
 )
@@ -66,6 +67,27 @@ func TestQueryAnnotations(t *testing.T) {
 	assert.Equal(t, "false", dep.Spec.Template.Annotations["sidecar.istio.io/inject"])
 	assert.Equal(t, "world", dep.Spec.Template.Annotations["hello"])
 	assert.Equal(t, "false", dep.Spec.Template.Annotations["prometheus.io/scrape"])
+}
+
+func TestQuerySecrets(t *testing.T) {
+	jaeger := v1alpha1.NewJaeger("TestQuerySecrets")
+	data := map[string]string{
+		"hello": "world",
+	}
+	secret := []v1.Secret{
+		v1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "mysecret",
+			},
+			StringData: data,
+		},
+	}
+	jaeger.Spec.Storage.Secrets.Items = secret
+
+	query := NewQuery(jaeger)
+	dep := query.Get()
+
+	assert.Equal(t, "mysecret", dep.Spec.Template.Spec.Containers[0].EnvFrom[0].SecretRef.LocalObjectReference.Name)
 }
 
 func TestQueryPodName(t *testing.T) {
