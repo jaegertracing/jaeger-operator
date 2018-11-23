4f8db72b2f00b68ad090b606cc2b667974a01a93
Juraci Paixão Kröhling <juraci@kroehling.de>
2018-11-23 11:34:28 +0100

    Install 'go dep'
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,6 +6,9 @@ go get github.com/securego/gosec/cmd/gosec/...
 echo "Installing golint"
 go get -u golang.org/x/lint/golint
 
+echo "Installing godep"
+go get -u github.com/golang/dep/cmd/dep
+
 echo "Installing operator-sdk"
 curl https://github.com/operator-framework/operator-sdk/releases/download/v0.1.1/operator-sdk-v0.1.1-x86_64-linux-gnu -sLo $GOPATH/bin/operator-sdk
 chmod +x $GOPATH/bin/operator-sdk
