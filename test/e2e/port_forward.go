712bda4b72c2f105e57c9641f6d8bf06113e9fe0
Pavol Loffay <ploffay@redhat.com>
2018-11-30 18:10:01 +0100

    Smoke tests
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

---
+++ test/e2e/port_forward.go
@@ -0,0 +1,43 @@
+package e2e
+
+import (
+	"bytes"
+	"fmt"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func CreatePortForward(namespace, pod string, ports []string, kConfig *rest.Config) (*portforward.PortForwarder, chan struct{}, error) {
+	roundTripper, upgrader, err := spdy.RoundTripperFor(kConfig)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	path := fmt.Sprintf("/api/v1/namespaces/%s/pods/%s/portforward", namespace, pod)
+	fmt.Println(kConfig.Host)
+	hostIP := strings.TrimLeft(kConfig.Host, "https://")
+	serverURL := url.URL{Scheme: "https", Path: path, Host: hostIP}
+
+	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: roundTripper}, http.MethodPost, &serverURL)
+
+	stopChan, readyChan := make(chan struct{}, 1), make(chan struct{}, 1)
+	out, errOut := new(bytes.Buffer), new(bytes.Buffer)
+	forwarder, err := portforward.New(dialer, ports, stopChan, readyChan, out, errOut)
+	if err != nil {
+		return nil, nil, err
+	}
+	go func() {
+		for range readyChan { // Kubernetes will close this channel when it has something to tell us.
+		}
+		if len(errOut.String()) != 0 {
+			panic(errOut.String())
+		} else if len(out.String()) != 0 {
+			fmt.Println(out.String())
+		}
+	}()
+	return forwarder, stopChan, nil
+}
