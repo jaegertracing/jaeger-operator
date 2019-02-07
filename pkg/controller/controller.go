86603ea7ab77910c3a7e518222b7f75768c44955
Juraci Paixão Kröhling <juraci.github@kroehling.de>
2019-02-07 11:10:06 +0100

    Added Route to SDK Scheme (#194)
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	routev1 "github.com/openshift/api/route/v1"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
@@ -9,6 +10,10 @@ var AddToManagerFuncs []func(manager.Manager) error
 
 // AddToManager adds all Controllers to the Manager
 func AddToManager(m manager.Manager) error {
+	if err := routev1.AddToScheme(m.GetScheme()); err != nil {
+		return err
+	}
+
 	for _, f := range AddToManagerFuncs {
 		if err := f(m); err != nil {
 			return err
