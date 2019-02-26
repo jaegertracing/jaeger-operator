65c69ffd3c2f9aa8f379f035b68ae4bbf47f4ef5
Greg Franklin <gregoryfranklin@rentalcars.com>
2019-02-26 14:16:38 +0000

    Add test checking SecretRef is added to es-index-cleaner spec
    Signed-off-by: Greg Franklin <gregoryfranklin@rentalcars.com>

--- pkg/cronjob/es_index_cleaner_test.go
+++ pkg/cronjob/es_index_cleaner_test.go
@@ -34,3 +34,12 @@ func TestCreateEsIndexCleaner(t *testing.T) {
 	assert.Equal(t, "INDEX_PREFIX", cronJob.Spec.JobTemplate.Spec.Template.Spec.Containers[0].Env[0].Name)
 	assert.Equal(t, "tenant1", cronJob.Spec.JobTemplate.Spec.Template.Spec.Containers[0].Env[0].Value)
 }
+
+func TestEsIndexCleanerSecrets(t *testing.T) {
+	jaeger := v1alpha1.NewJaeger("TestEsIndexCleanerSecrets")
+	secret := "mysecret"
+	jaeger.Spec.Storage.SecretName = secret
+
+	cronJob := CreateEsIndexCleaner(jaeger)
+	assert.Equal(t, secret, cronJob.Spec.JobTemplate.Spec.Template.Spec.Containers[0].EnvFrom[0].SecretRef.LocalObjectReference.Name)
+}
