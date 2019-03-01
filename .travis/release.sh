f2336878d5e0b0553211403aecbafb8235dd3cb0
Juraci Paixão Kröhling <juraci.github@kroehling.de>
2019-03-01 11:06:16 +0100

    Fixed the version bump during the release in deploy artifacts (#247)
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- .travis/release.sh
+++ .travis/release.sh
@@ -12,14 +12,32 @@ OPERATOR_VERSION=$(echo ${OPERATOR_VERSION} | grep -Po "([\d\.]+)")
 TAG=${TAG:-"v${OPERATOR_VERSION}"}
 BUILD_IMAGE=${BUILD_IMAGE:-"${BASE_BUILD_IMAGE}:${OPERATOR_VERSION}"}
 
-sed "s~image: jaegertracing\/jaeger-operator\:.*~image: ${BUILD_IMAGE}~gi" -i deploy/operator.yaml
-sed "s~image: jaegertracing\/jaeger-operator\:.*~image: ${BUILD_IMAGE}~gi" -i deploy/operator-openshift.yaml
+# changes to deploy/operator.yaml
+sed "s~image: jaegertracing/jaeger-operator.*~image: ${BUILD_IMAGE}~gi" -i deploy/operator.yaml
+
+# changes to deploy/olm-catalog/jaeger.package.yaml
+sed "s/currentCSV: jaeger-operator.*/currentCSV: jaeger-operator.v${OPERATOR_VERSION}/gi" -i deploy/olm-catalog/jaeger.package.yaml
+
+# changes to deploy/olm-catalog/jaeger-operator.csv.yaml
+sed "s~containerImage: docker.io/jaegertracing/jaeger-operator.*~containerImage: docker.io/${BUILD_IMAGE}~gi" -i deploy/olm-catalog/jaeger-operator.csv.yaml
+sed "s/name: jaeger-operator\.v.*/name: jaeger-operator.v${OPERATOR_VERSION}/gi" -i deploy/olm-catalog/jaeger-operator.csv.yaml
+sed "s~image: jaegertracing/jaeger-operator.*~image: ${BUILD_IMAGE}~gi" -i deploy/olm-catalog/jaeger-operator.csv.yaml
+## there's a "version: v1alpha1" there somewhere...
+sed -E "s/version: ([0-9\.]+).*/version: ${OPERATOR_VERSION}/gi" deploy/olm-catalog/jaeger-operator.csv.yaml
+
+# changes to test/operator.yaml
+sed "s~image: jaegertracing/jaeger-operator.*~image: ${BUILD_IMAGE}~gi" -i test/operator.yaml
 
 git diff -s --exit-code
 if [[ $? == 0 ]]; then
     echo "No changes detected. Skipping."
 else
-    git add deploy/operator.yaml deploy/operator-openshift.yaml
+    git add \
+      deploy/operator.yaml \
+      deploy/olm-catalog/jaeger.package.yaml \
+      deploy/olm-catalog/jaeger-operator.csv.yaml \
+      test/operator.yaml
+
     git commit -qm "Release ${TAG}" --author="Jaeger Release <jaeger-release@jaegertracing.io>"
     git tag ${TAG}
     git push --repo=https://${GH_WRITE_TOKEN}@github.com/jaegertracing/jaeger-operator.git --tags
