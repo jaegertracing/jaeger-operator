6cc3c89440c7cccae716279831842bab18f853f6
Juraci Paixão Kröhling <juraci.github@kroehling.de>
2019-03-06 16:49:49 +0100

    Added publishing of major.minor image for the operator (#274)
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- .travis/publish-images.sh
+++ .travis/publish-images.sh
@@ -8,6 +8,7 @@ OPERATOR_VERSION=${OPERATOR_VERSION:-$(git describe --tags)}
 ## if we are not running in travis, it fallsback to the `git describe` above
 if [[ $OPERATOR_VERSION == release* ]]; then
     OPERATOR_VERSION=$(echo ${OPERATOR_VERSION} | grep -Po "([\d\.]+)")
+    MAJOR_MINOR=$(echo ${OPERATOR_VERSION} | awk -F. '{print $1"."$2}')
 fi
 
 BUILD_IMAGE=${BUILD_IMAGE:-"${BASE_BUILD_IMAGE}:${OPERATOR_VERSION}"}
@@ -19,3 +20,9 @@ fi
 
 echo "Building and publishing image ${BUILD_IMAGE}"
 make build docker push BUILD_IMAGE=${BUILD_IMAGE}
+
+if [ "${MAJOR_MINOR}x" != "x" ]; then
+    MAJOR_MINOR_IMAGE="${BASE_BUILD_IMAGE}:${MAJOR_MINOR}"
+    docker tag "${BUILD_IMAGE}" "${MAJOR_MINOR_IMAGE}"
+    docker push "${MAJOR_MINOR_IMAGE}"
+fi
