a3904c2bd714224a351c79e9611f60516676649d
Kevin Earls <kearls@redhat.com>
2019-04-05 11:35:52 +0200

    Fix docker login
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- .travis/rune2eTests.sh
+++ .travis/rune2eTests.sh
@@ -5,4 +5,11 @@ kubectl get all --all-namespaces
 kubectl get ingress --all-namespaces
 minikube ip
 
+if [ "${DOCKER_PASSWORD}x" != "x" -a "${DOCKER_USERNAME}x" != "x" ]; then
+    echo "Performing a 'docker login'"
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    export NAMESPACE=${DOCKER_USERNAME}
+fi
+
+
 make e2e-tests-smoke
