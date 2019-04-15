b10390cc75f66a3e3f2b79cfd88eb192c7d4fe06
Kevin Earls <kearls@redhat.com>
2019-04-15 10:22:53 +0200

    Update minikube and kubernetes versions
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- .travis/setupMinikube.sh
+++ .travis/setupMinikube.sh
@@ -8,12 +8,17 @@ set -u
 # print each command before executing it
 set -x
 
+#
+# NOTE: This script was riginally copied from the CoreOs Prometheus Operator build
+# https://github.com/coreos/prometheus-operator/blob/master/scripts/create-minikube.sh
+# I'm not sure we need everying in it
+
 # Needed for port forwarding
 sudo apt-get update && sudo apt-get install socat
 
 #TODO Update these versions
-export MINIKUBE_VERSION=v0.35.0
-export KUBERNETES_VERSION=v1.13.4
+export MINIKUBE_VERSION=v1.0.0
+export KUBERNETES_VERSION=v1.14.0
 
 ls -alF
 # TODO Do we need these
@@ -21,11 +26,11 @@ sudo mount --make-rshared /
 sudo mount --make-rshared /proc
 sudo mount --make-rshared /sys
 
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$KUBERNETES_VERSION/bin/linux/amd64/kubectl && \
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBERNETES_VERSION}/bin/linux/amd64/kubectl && \
     chmod +x kubectl &&  \
     sudo mv kubectl /usr/local/bin/
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.35.0/minikube-linux-amd64 && \
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && \
     chmod +x minikube &&  \
     sudo mv minikube /usr/local/bin/minikube
 
@@ -58,9 +63,6 @@ JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.ty
 
 # waiting for kube-dns to be ready
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl -n kube-system get pods -lk8s-app=kube-dns -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1;echo "waiting for kube-dns to be available"; kubectl get pods --all-namespaces; done
-
-# TODO Do we need this?
-#kubectl apply -f scripts/minikube-rbac.yaml
 
 sudo minikube addons enable ingress
 
