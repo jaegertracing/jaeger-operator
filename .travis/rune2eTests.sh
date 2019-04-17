7681a0ab99c84c6f7a69befc44a8046ab85004e3
Kevin Earls <kearls@redhat.com>
2019-04-17 17:47:25 +0200

    Try running tests in parallel
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- .travis/rune2eTests.sh
+++ .travis/rune2eTests.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
 set -x
+
+[[ -z "$TEST_GROUP" ]] && { echo "TEST_GROUP is undefined, exiting" ; exit 1; }
+
 # Confirm we're working
 kubectl get all --all-namespaces
 
 ## Since we're running MiniKube with --vm-driver none, change imagePullPolicy to get the image locally
 sed -i 's/imagePullPolicy: Always/imagePullPolicy: Never/g' test/operator.yaml
 
-# Do these first to avoid race conditions
-make cassandra
-make es
 
-make e2e-tests
+if [ "${TEST_GROUP}" = "es" ]; then
+    echo "Running elasticsearch tests"
+    make es
+    # TODO wait?
+    make e2e-tests-es
+elif [ "${TEST_GROUP}" = "smoke" ]
+then
+    echo "Running Smoke Tests"
+    make e2e-tests-smoke
+elif [ "${TEST_GROUP}" = "cassandra" ]
+then
+    echo "Running Cassandra Tests"
+    make cassandra
+    # TODO wait?
+    make e2e-tests-cassandra
+else
+    echo "Unknown TEST_GROUP [${TEST_GROUP}]"; exit 1
+fi
 
