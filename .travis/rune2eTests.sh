bca9103a0ee1524644364f37b5e90f01fb314b6f
Kevin Earls <kearls@redhat.com>
2019-04-18 15:02:54 +0200

    Enable running e2e tests on a per PR basis on Travis (#376)
    * Enable running e2e tests on a per PR basis on Travis
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Try running tests in parallel
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Start build and e2e tests at the same time
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Cleanup unused code, comments
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Fix alignment
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>

---
+++ .travis/rune2eTests.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -x
+
+[[ -z "$TEST_GROUP" ]] && { echo "TEST_GROUP is undefined, exiting" ; exit 1; }
+
+## Since we're running MiniKube with --vm-driver none, change imagePullPolicy to get the image locally
+sed -i 's/imagePullPolicy: Always/imagePullPolicy: Never/g' test/operator.yaml
+
+if [ "${TEST_GROUP}" = "es" ]; then
+    echo "Running elasticsearch tests"
+    make es
+    make e2e-tests-es
+elif [ "${TEST_GROUP}" = "smoke" ]
+then
+    echo "Running Smoke Tests"
+    make e2e-tests-smoke
+elif [ "${TEST_GROUP}" = "cassandra" ]
+then
+    echo "Running Cassandra Tests"
+    make cassandra
+    make e2e-tests-cassandra
+else
+    echo "Unknown TEST_GROUP [${TEST_GROUP}]"; exit 1
+fi
+
