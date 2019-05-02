143b4625d8344edc1c2dfcdf2362bbc63ffde466
Kevin Earls <kearls@redhat.com>
2019-05-02 15:42:42 +0200

    First part of test refactoring to improve reporting
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- test/e2e/daemonset_test.go
+++ test/e2e/daemonset_test.go
@@ -1,9 +1,10 @@
+// +build smoke
+
 package e2e
 
 import (
 	goctx "context"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os/exec"
@@ -11,6 +12,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 	osv1sec "github.com/openshift/api/security/v1"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
@@ -24,74 +27,68 @@ import (
 	"github.com/jaegertracing/jaeger-operator/pkg/apis/jaegertracing/v1"
 )
 
-// DaemonSet runs a test with the agent as DaemonSet
-func DaemonSet(t *testing.T) {
-	ctx := prepare(t)
-	defer ctx.Cleanup()
 
-	if err := daemonsetTest(t, framework.Global, ctx); err != nil {
-		t.Fatal(err)
-	}
+
+type DaemonSetTestSuite struct {
+	suite.Suite
 }
 
-func daemonsetTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) error {
+func(suite *DaemonSetTestSuite) SetupSuite() {
+	t = suite.T()
+	ctx = prepare(t)
+	fw = framework.Global
+	namespace, _ = ctx.GetNamespace()
+	require.NotNil(t, namespace, "GetNamespace failed")
+
+	addToFrameworkSchemeForSmokeTests(t)
+}
+
+func (suite *DaemonSetTestSuite) TearDownSuite() {
+	log.Info("Entering TearDownSuite()")
+	ctx.Cleanup()
+}
+
+func TestDaemonSetSuite(t *testing.T) {
+	suite.Run(t, new(DaemonSetTestSuite))
+}
+
+// DaemonSet runs a test with the agent as DaemonSet
+func (suite *DaemonSetTestSuite) TestDaemonSet()  {
 	cleanupOptions := &framework.CleanupOptions{TestContext: ctx, Timeout: timeout, RetryInterval: retryInterval}
-	namespace, err := ctx.GetNamespace()
-	if err != nil {
-		return fmt.Errorf("could not get namespace: %v", err)
-	}
 
 	if isOpenShift(t) {
-		err = f.Client.Create(goctx.TODO(), hostPortSccDaemonset(), cleanupOptions)
+		err := fw.Client.Create(goctx.TODO(), hostPortSccDaemonset(), cleanupOptions)
 		if err != nil && !strings.Contains(err.Error(), "already exists") {
-			return err
+			t.Fatalf("Failed creating hostPortSccDaemonset %v\n", err)
 		}
 
 		// ideally, we would use the REST API, but for a single-usage within the project, this is the simplest solution that works
 		cmd := exec.Command("oc", "adm", "--namespace", namespace, "policy",  "add-scc-to-user", "daemonset-with-hostport", "-z", "default")
 		output, err := cmd.CombinedOutput()
-		if err != nil {
-			fmt.Printf("Failed creating hostport scc with error [%v] OUTPUT: [%v]\n", err, string(output))
-			return err;
-		}
+		require.NoError(t, err,"Failed creating hostport scc with OUTPUT: [%s]\n", string(output) )
 	}
 
 	j := jaegerAgentAsDaemonsetDefinition(namespace, "agent-as-daemonset")
 	log.Infof("passing %v", j)
-	err = f.Client.Create(goctx.TODO(), j, cleanupOptions)
-	if err != nil {
-		return err
-	}
+	err := fw.Client.Create(goctx.TODO(), j, cleanupOptions)
+	require.NoError(t, err, "Error deploying jaeger")
 
-	err = WaitForDaemonSet(t, f.KubeClient, namespace, "agent-as-daemonset-agent-daemonset", retryInterval, timeout)
-	if err != nil {
-		return err
-	}
+	err = WaitForDaemonSet(t, fw.KubeClient, namespace, "agent-as-daemonset-agent-daemonset", retryInterval, timeout)
+	require.NoError(t, err, "Error waiting for daemonset to startup")
 
 	selector := map[string]string{"app": "vertx-create-span"}
 	dep := getVertxDeployment(namespace, selector)
-	err = f.Client.Create(goctx.TODO(), dep, cleanupOptions)
-	if err != nil {
-		return err
-	}
+	err = fw.Client.Create(goctx.TODO(), dep, cleanupOptions)
+	require.NoError(t, err, "Error creating VertX app")
 
-	err = e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "vertx-create-span", 1, retryInterval, 3 * timeout)
-	if err != nil {
-		fmt.Println("Failed waiting for vertx deployment")
-		return err
-	}
+	err = e2eutil.WaitForDeployment(t, fw.KubeClient, namespace, "vertx-create-span", 1, retryInterval, timeout)
+	require.NoError(t, err, "Error waiting for VertX app to start")
 
-	queryPod, err := GetPod(namespace, "agent-as-daemonset", "jaegertracing/all-in-one", f.KubeClient)
-	if err != nil {
-		fmt.Println("Failed waiting for pod agent-as-daemonset")
-		return err
-	}
+	queryPod, err := GetPod(namespace, "agent-as-daemonset", "jaegertracing/all-in-one", fw.KubeClient)
+	require.NoErrorf(t, err, "Error trying to find pod with prefix agent-as-daemonset and image jaegertracing/all-in-one in namespace [%s]: %s\n", namespace)
 
-	portForw, closeChan, err := CreatePortForward(namespace, queryPod.Name, []string{"16686"}, f.KubeConfig)
-	if err != nil {
-		fmt.Println("Failed waiting for port forward")
-		return err
-	}
+	portForw, closeChan, err := CreatePortForward(namespace, queryPod.Name, []string{"16686"}, fw.KubeConfig)
+	require.NoError(t, err, "Error creating portforward")
 	defer portForw.Close()
 	defer close(closeChan)
 
@@ -99,12 +96,9 @@ func daemonsetTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx)
 	c := http.Client{Timeout: time.Second}
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		fmt.Println("Failed waiting for get on " + url)
-		return err
-	}
+	require.NoError(t, err, "Failed to create httpRequest")
 
-	return wait.Poll(retryInterval, timeout, func() (done bool, err error) {
+	err =  wait.Poll(retryInterval, timeout, func() (done bool, err error) {
 		res, err := c.Do(req)
 		if err != nil {
 			return false, err
@@ -123,6 +117,7 @@ func daemonsetTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx)
 
 		return len(resp.Data) > 0, nil
 	})
+	require.NoError(t, err, "Failed waiting for expected content")
 }
 
 func getVertxDeployment(namespace string, selector map[string]string) *appsv1.Deployment {
---
+++ test/e2e/sidecar_test.go
@@ -0,0 +1,178 @@
+// +build smoke
+
+package e2e
+
+import (
+	goctx "context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+	log "github.com/sirupsen/logrus"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/wait"
+
+	"github.com/jaegertracing/jaeger-operator/pkg/apis/jaegertracing/v1"
+	"github.com/jaegertracing/jaeger-operator/pkg/inject"
+)
+
+type SidecarTestSuite struct {
+	suite.Suite
+}
+
+func(suite *SidecarTestSuite) SetupSuite() {
+	t = suite.T()
+	ctx = prepare(t)
+	fw = framework.Global
+	namespace, _ = ctx.GetNamespace()
+	require.NotNil(t, namespace, "GetNamespace failed")
+
+	addToFrameworkSchemeForSmokeTests(t)
+}
+
+func (suite *SidecarTestSuite) TearDownSuite() {
+	log.Info("Entering TearDownSuite()")
+	ctx.Cleanup()
+}
+
+func TestSidecarSuite(t *testing.T) {
+	suite.Run(t, new(SidecarTestSuite))
+}
+
+// Sidecar runs a test with the agent as sidecar
+func (suite *SidecarTestSuite) TestSidecar() {
+	cleanupOptions := &framework.CleanupOptions{TestContext: ctx, Timeout: timeout, RetryInterval: retryInterval}
+
+	j := getJaegerAgentAsSidecarDefinition(namespace)
+	err := fw.Client.Create(goctx.TODO(), j, cleanupOptions)
+	require.NoError(t, err, "Failed to create jaeger instance")
+
+	dep := getVertxDefinition(namespace)
+	err = fw.Client.Create(goctx.TODO(), dep, cleanupOptions)
+	require.NoError(t, err, "Failed to create vertx instance")
+
+	err = e2eutil.WaitForDeployment(t, fw.KubeClient, namespace, "vertx-create-span-sidecar", 1, retryInterval, timeout)
+	require.NoError(t, err, "Failed waiting for vertx-create-span-sidecar deployment")
+
+	queryPod, err := GetPod(namespace, "agent-as-sidecar", "jaegertracing/all-in-one", fw.KubeClient)
+	require.NoError(t, err, "Failed to find pod starting with agent-as-sidecar with image jaegertracing/all-in-one")
+
+	portForward, closeChan, err := CreatePortForward(namespace, queryPod.Name, []string{"16686"}, fw.KubeConfig)
+	require.NoError(t, err, "Failed to create PortForward")
+	defer portForward.Close()
+	defer close(closeChan)
+
+	url := "http://localhost:16686/api/traces?service=order"
+	c := http.Client{Timeout: time.Second}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	require.NoError(t, err, "Failed to create httpRequest")
+
+	err = wait.Poll(retryInterval, timeout, func() (done bool, err error) {
+		res, err := c.Do(req)
+		if err != nil {
+			return false, err
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return false, err
+		}
+
+		resp := &resp{}
+		err = json.Unmarshal(body, &resp)
+		if err != nil {
+			return false, err
+		}
+
+		return len(resp.Data) > 0, nil
+	})
+	require.NoError(t, err, "Failed waiting for expected content")
+}
+
+func getVertxDefinition(s string) *appsv1.Deployment {
+	selector := map[string]string{"app": "vertx-create-span-sidecar"}
+	dep := &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "vertx-create-span-sidecar",
+			Namespace:   namespace,
+			Annotations: map[string]string{inject.Annotation: "true"},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: selector,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: selector,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Image: "jaegertracing/vertx-create-span:operator-e2e-tests",
+						Name:  "vertx-create-span-sidecar",
+						Ports: []corev1.ContainerPort{
+							{
+								ContainerPort: 8080,
+							},
+						},
+						ReadinessProbe: &corev1.Probe{
+							Handler: corev1.Handler{
+								HTTPGet: &corev1.HTTPGetAction{
+									Path: "/",
+									Port: intstr.FromInt(8080),
+								},
+							},
+							InitialDelaySeconds: 1,
+						},
+						LivenessProbe: &corev1.Probe{
+							Handler: corev1.Handler{
+								HTTPGet: &corev1.HTTPGetAction{
+									Path: "/",
+									Port: intstr.FromInt(8080),
+								},
+							},
+							InitialDelaySeconds: 1,
+						},
+					}},
+				},
+			},
+		},
+	}
+	return dep
+}
+
+func getJaegerAgentAsSidecarDefinition(namespace string) *v1.Jaeger {
+	j := &v1.Jaeger{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Jaeger",
+			APIVersion: "jaegertracing.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "agent-as-sidecar",
+			Namespace: namespace,
+		},
+		Spec: v1.JaegerSpec{
+			Strategy: "allInOne",
+			AllInOne: v1.JaegerAllInOneSpec{},
+			Agent: v1.JaegerAgentSpec{
+				Options: v1.NewOptions(map[string]interface{}{
+					"log-level": "debug",
+				}),
+			},
+		},
+	}
+	return j
+}
