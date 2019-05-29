161b7ef98e038c368ff184985668c769593f3233
Juraci Paixão Kröhling <juraci@kroehling.de>
2019-05-29 13:59:19 +0200

    Switch to go modules from dep
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,5 +7,5 @@ echo "Installing golint"
 go get -u golang.org/x/lint/golint
 
 echo "Installing operator-sdk"
-curl https://github.com/operator-framework/operator-sdk/releases/download/v0.5.0/operator-sdk-v0.5.0-x86_64-linux-gnu -sLo $GOPATH/bin/operator-sdk
+curl https://github.com/operator-framework/operator-sdk/releases/download/v0.8.1/operator-sdk-v0.8.1-x86_64-linux-gnu -sLo $GOPATH/bin/operator-sdk
 chmod +x $GOPATH/bin/operator-sdk
