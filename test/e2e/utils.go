e32a7f9fa2f9527675857cee6ce4445039b6173a
Kevin Earls <kearls@redhat.com>
2019-07-11 15:01:16 +0200

    Wait for jaeger instance to finish deleting
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- test/e2e/utils.go
+++ test/e2e/utils.go
@@ -177,6 +177,8 @@ func printTestStackTrace() {
 func undeployJaegerInstance(jaeger *v1.Jaeger) {
 	err := fw.Client.Delete(goctx.TODO(), jaeger)
 	require.NoError(t, err, "Error undeploying Jaeger")
+	err = e2eutil.WaitForDeletion(t, fw.Client.Client, jaeger, retryInterval, timeout)
+	require.NoError(t, err)
 }
 
 type resp struct {
