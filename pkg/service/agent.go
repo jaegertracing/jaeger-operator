54c0cf7a0c11fbcabdda44102cc8a7b6143e2905
Dawid Nowak <dawid.nowak@relayr.io>
2019-08-05 13:17:51 +0200

    Agent service ports with correct protocol (#579)
    Signed-off-by: Dawid Nowak <dawid.nowak@relayr.io>

--- pkg/service/agent.go
+++ pkg/service/agent.go
@@ -45,20 +45,23 @@ func NewAgentService(jaeger *v1.Jaeger, selector map[string]string) *corev1.Serv
 			ClusterIP: "None",
 			Ports: []corev1.ServicePort{
 				{
-					Name: "zk-compact-trft",
-					Port: 5775,
+					Name:     "zk-compact-trft",
+					Port:     5775,
+					Protocol: corev1.ProtocolUDP,
 				},
 				{
 					Name: "config-rest",
 					Port: 5778,
 				},
 				{
-					Name: "jg-compact-trft",
-					Port: 6831,
+					Name:     "jg-compact-trft",
+					Port:     6831,
+					Protocol: corev1.ProtocolUDP,
 				},
 				{
-					Name: "jg-binary-trft",
-					Port: 6832,
+					Name:     "jg-binary-trft",
+					Port:     6832,
+					Protocol: corev1.ProtocolUDP,
 				},
 			},
 		},
