a020b5b8404855c99821403b065a5484f982ab52
Juraci Paixão Kröhling <juraci@kroehling.de>
2019-09-04 15:23:11 +0200

    Update changelog for v1.14.0 (#641)
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- pkg/upgrade/v1_14_0.go
+++ pkg/upgrade/v1_14_0.go
@@ -6,7 +6,7 @@ import (
 	v1 "github.com/jaegertracing/jaeger-operator/pkg/apis/jaegertracing/v1"
 )
 
-func upgrade1_13_1(client client.Client, jaeger v1.Jaeger) (v1.Jaeger, error) {
+func upgrade1_14_0(client client.Client, jaeger v1.Jaeger) (v1.Jaeger, error) {
 	// this has the same content as `noop`, but it's added a separate function
 	// to serve as template for versions with an actual upgrade procedure
 	return jaeger, nil
