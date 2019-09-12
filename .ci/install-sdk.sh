62068cac1be3515d6bc12913b61d85c9d48dfee9
Kevin Earls <kearls@redhat.com>
2019-09-12 11:36:13 +0200

    Update install-sdk to work on Mac
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- .ci/install-sdk.sh
+++ .ci/install-sdk.sh
@@ -4,7 +4,11 @@ DEST="${GOPATH}/bin/operator-sdk"
 
 function install_sdk() {
     echo "Downloading the operator-sdk ${SDK_VERSION} into ${DEST}"
-    curl https://github.com/operator-framework/operator-sdk/releases/download/${SDK_VERSION}/operator-sdk-${SDK_VERSION}-x86_64-linux-gnu -sLo ${DEST}
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        curl https://github.com/operator-framework/operator-sdk/releases/download/${SDK_VERSION}/operator-sdk-${SDK_VERSION}-x86_64-apple-darwin -sLo ${DEST}
+    else
+        curl https://github.com/operator-framework/operator-sdk/releases/download/${SDK_VERSION}/operator-sdk-${SDK_VERSION}-x86_64-linux-gnu -sLo ${DEST}
+    fi
     chmod +x ${DEST}
 }
 
