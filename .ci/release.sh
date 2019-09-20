1131bebf7f747b8ee4214da8e9a0f99a76891191
Juraci Paixão Kröhling <juraci@kroehling.de>
2019-09-20 10:05:45 +0200

    release
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- .ci/release.sh
+++ .ci/release.sh
@@ -14,6 +14,7 @@ JAEGER_VERSION=$(echo ${OPERATOR_VERSION} | grep -Po "([\d]+\.[\d]+\.[\d]+)" | h
 TAG=${TAG:-"v${OPERATOR_VERSION}"}
 BUILD_IMAGE=${BUILD_IMAGE:-"${BASE_BUILD_IMAGE}:${OPERATOR_VERSION}"}
 CREATED_AT=$(date -u -Isecond)
+PREVIOUS_VERSION=$(grep -v \# jaeger-operator.version)
 
 if [[ ${BASE_TAG} =~ ^release/v.[[:digit:].]+(\-.*)?$ ]]; then
     echo "Releasing ${OPERATOR_VERSION} from ${BASE_TAG}"
@@ -27,29 +28,20 @@ if [ "${GH_WRITE_TOKEN}x" == "x" ]; then
     exit 1
 fi
 
+operator-sdk olm-catalog gen-csv \
+    --operator-name=jaeger
+    --csv-channel=stable \
+    --csv-version=${OPERATOR_VERSION} \
+    --from-version=${PREVIOUS_VERSION} \
+
 # changes to deploy/operator.yaml
 sed "s~image: jaegertracing/jaeger-operator.*~image: ${BUILD_IMAGE}~gi" -i deploy/operator.yaml
 
-# changes to deploy/olm-catalog/jaeger.package.yaml
-sed "s/currentCSV: jaeger-operator.*/currentCSV: jaeger-operator.v${OPERATOR_VERSION}/gi" -i deploy/olm-catalog/jaeger.package.yaml
-
-# changes to deploy/olm-catalog/jaeger.clusterserviceversion.yaml
-sed "s~containerImage: docker.io/jaegertracing/jaeger-operator.*~containerImage: docker.io/${BUILD_IMAGE}~gi" -i deploy/olm-catalog/jaeger.clusterserviceversion.yaml
-sed "s/name: jaeger-operator\.v.*/name: jaeger-operator.v${OPERATOR_VERSION}/gi" -i deploy/olm-catalog/jaeger.clusterserviceversion.yaml
-sed "s~image: jaegertracing/jaeger-operator.*~image: ${BUILD_IMAGE}~gi" -i deploy/olm-catalog/jaeger.clusterserviceversion.yaml
-
-sed "s/all-in-one:.*\"/all-in-one:${JAEGER_VERSION}\"/gi" -i deploy/olm-catalog/jaeger.clusterserviceversion.yaml
-
-sed "s/createdAt: .*/createdAt: \"${CREATED_AT}\"/gi" -i deploy/olm-catalog/jaeger.clusterserviceversion.yaml
-
-export PREVIOUS_OPERATOR_VERSION=`grep "version: [0-9]" deploy/olm-catalog/jaeger.clusterserviceversion.yaml | cut -f4 -d' '`
-sed "s/replaces: jaeger-operator\.v.*/replaces: jaeger-operator.v${PREVIOUS_OPERATOR_VERSION}/gi" -i deploy/olm-catalog/jaeger.clusterserviceversion.yaml
-
-## there's a "version: v1" there somewhere that we want to avoid
-sed -E "s/version: ([0-9\.]+).*/version: ${OPERATOR_VERSION}/gi" -i deploy/olm-catalog/jaeger.clusterserviceversion.yaml
-
 # changes to test/operator.yaml
 sed "s~image: jaegertracing/jaeger-operator.*~image: ${BUILD_IMAGE}~gi" -i test/operator.yaml
+
+# change the jaeger-operator.version
+sed "s~${PREVIOUS_VERSION}~${OPERATOR_VERSION}" -i jaeger-operator.version
 
 git diff -s --exit-code
 if [[ $? == 0 ]]; then
@@ -61,8 +53,8 @@ else
       deploy/olm-catalog/jaeger.clusterserviceversion.yaml \
       test/operator.yaml
 
-    git commit -qm "Release ${TAG}" --author="Jaeger Release <jaeger-release@jaegertracing.io>"
-    git tag ${TAG}
-    git push --repo=https://${GH_WRITE_TOKEN}@github.com/jaegertracing/jaeger-operator.git --tags
-    git push https://${GH_WRITE_TOKEN}@github.com/jaegertracing/jaeger-operator.git refs/tags/${TAG}:master
+    # git commit -qm "Release ${TAG}" --author="Jaeger Release <jaeger-release@jaegertracing.io>"
+    # git tag ${TAG}
+    # git push --repo=https://${GH_WRITE_TOKEN}@github.com/jaegertracing/jaeger-operator.git --tags
+    # git push https://${GH_WRITE_TOKEN}@github.com/jaegertracing/jaeger-operator.git refs/tags/${TAG}:master
 fi
