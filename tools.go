acf7c58cae3d413a7b706939654ba3b197ab3df7
Juraci Paixão Kröhling <juraci@kroehling.de>
2019-11-20 15:09:10 +0100

    Cleaned up tools.go
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- tools.go
+++ tools.go
@@ -1,16 +1,5 @@
 // +build tools
 
+// Place any runtime dependencies as imports in this file.
+// Go modules will be forced to download and install them.
 package tools
-
-import (
-	// Code generators built at runtime.
-
-	_ "k8s.io/code-generator/cmd/client-gen"
-	_ "k8s.io/code-generator/cmd/conversion-gen"
-	_ "k8s.io/code-generator/cmd/deepcopy-gen"
-	_ "k8s.io/code-generator/cmd/informer-gen"
-	_ "k8s.io/code-generator/cmd/lister-gen"
-	_ "k8s.io/gengo/args"
-	_ "k8s.io/kube-openapi/cmd/openapi-gen"
-	_ "sigs.k8s.io/controller-tools/pkg/crd/generator"
-)
