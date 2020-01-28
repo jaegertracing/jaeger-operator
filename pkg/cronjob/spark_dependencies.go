c0d5e7e0374d409dd2d9b65b271c831ba6e9f229
Pavol Loffay <ploffay@redhat.com>
2020-01-28 17:08:05 +0100

    Do not use resources from common spec in spark dependencies
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- pkg/cronjob/spark_dependencies.go
+++ pkg/cronjob/spark_dependencies.go
@@ -87,7 +87,7 @@ func CreateSparkDependencies(jaeger *v1.Jaeger) *batchv1beta1.CronJob {
 									// let spark job use its default values
 									Env:       util.RemoveEmptyVars(envVars),
 									EnvFrom:   envFromSource,
-									Resources: commonSpec.Resources,
+									Resources: jaeger.Spec.Storage.Dependencies.Resources,
 								},
 							},
 							RestartPolicy:      corev1.RestartPolicyNever,
