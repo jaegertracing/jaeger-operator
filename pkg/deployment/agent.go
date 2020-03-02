c1bb2b3f39f8ed8272449d07ae8868ceeb56b69b
Annanay <annanayagarwal@gmail.com>
2020-03-02 15:01:55 +0530

    Explicitly add TLS server name in agent connection
    Signed-off-by: Annanay <annanayagarwal@gmail.com>

--- pkg/deployment/agent.go
+++ pkg/deployment/agent.go
@@ -43,6 +43,7 @@ func (a *Agent) Get() *appsv1.DaemonSet {
 		// we only add the grpc host if we are adding the reporter type and there's no explicit value yet
 		if len(util.FindItem("--reporter.grpc.host-port=", args)) == 0 {
 			args = append(args, fmt.Sprintf("--reporter.grpc.host-port=dns:///%s.%s:14250", service.GetNameForHeadlessCollectorService(a.jaeger), a.jaeger.Namespace))
+			args = append(args, fmt.Sprintf("--reporter.grpc.tls.server-name=%s", service.GetNameForHeadlessCollectorService(a.jaeger)))
 		}
 
 		// Enable tls by default for openshift platform
