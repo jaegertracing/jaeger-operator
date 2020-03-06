1747c625ff8972fa7b2eae3db3e27f1d92fb1fd9
Kevin Earls <kearls@redhat.com>
2020-03-06 14:37:50 +0100

    Added workaround for operator deployment timeouts until #947 is fixed
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- test/e2e/utils.go
+++ test/e2e/utils.go
@@ -139,7 +139,8 @@ func prepare(t *testing.T) (*framework.TestCtx, error) {
 	f := framework.Global
 	// wait for the operator to be ready
 	if !usingOLM {
-		err := e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "jaeger-operator", 1, retryInterval, timeout)
+		// TODO replace 10*time.minute with timeout once #947 is fixed
+		err := e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "jaeger-operator", 1, retryInterval, 10*time.Minute)
 		if err != nil {
 			return nil, err
 		}
