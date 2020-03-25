cc2cff4a1f940a75999d2f887a981636673be852
Juraci Paixão Kröhling <juraci@kroehling.de>
2020-03-25 09:24:00 +0100

    Change 'make generate' to write only a single CRD (#978)
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- .ci/generate.sh
+++ .ci/generate.sh
@@ -35,6 +35,20 @@ fi
 # move the generated CRD to the same location the operator-sdk places
 mv deploy/crds/jaegertracing.io_jaegers.yaml deploy/crds/jaegertracing.io_jaegers_crd.yaml
 
+# the controller-gen will generate a list of CRDs, but the operator-sdk tooling expects
+# a single item
+# the proper solutions are, in order:
+# 1) find a controller-gen switch that makes it write only one CRD. Such a switch doesn't exist yet: https://git.io/JvX5D
+# 2) use a YAML command line tool to get the first item from the file
+# 3) chop off the first two lines of the file
+# the last option is the easiest to implement for now, also because `tail` is found everywhere
+echo "$(tail -n +3 deploy/crds/jaegertracing.io_jaegers_crd.yaml)" > deploy/crds/jaegertracing.io_jaegers_crd.yaml
+
+if ! [[ "$(head -n 1 deploy/crds/jaegertracing.io_jaegers_crd.yaml)" == "apiVersion"* ]]; then
+    echo "The generated CRD doesn't seem valid. Make sure the controller-gen is generating the CRD in the expected format. Aborting."
+    exit 1
+fi
+
 # generate the schema validation (openapi) stubs
 ${OPENAPIGEN} --logtostderr=true -o "" -i ./pkg/apis/jaegertracing/v1 -O zz_generated.openapi -p ./pkg/apis/jaegertracing/v1 -h /dev/null -r "-"
 RT=$?
