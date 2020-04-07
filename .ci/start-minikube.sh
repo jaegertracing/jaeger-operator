e50cfba0a17f2952a45420a728900f6a004f88be
Kevin Earls <kearls@redhat.com>
2020-04-07 16:43:33 +0200

    Try newer minikube and kubernetes versions
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- .ci/start-minikube.sh
+++ .ci/start-minikube.sh
@@ -15,8 +15,8 @@ set -x
 # socat is needed for port forwarding
 sudo apt-get update && sudo apt-get install socat
 
-export MINIKUBE_VERSION=v1.2.0
-export KUBERNETES_VERSION=v1.14.5
+export MINIKUBE_VERSION=v1.9.2
+export KUBERNETES_VERSION=v1.18.0
 
 MINIKUBE=$(which minikube) # it's outside of the regular PATH, so, need the full path when calling with sudo
 
