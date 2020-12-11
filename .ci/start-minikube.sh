58cfa205110b72aa65b633c09b6ce9fd21dce3d2
Kevin Earls <kearls@redhat.com>
2020-12-11 16:02:22 +0100

    Remove wait for addons manager
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- .ci/start-minikube.sh
+++ .ci/start-minikube.sh
@@ -44,12 +44,9 @@ minikube update-context
 # waiting for node(s) to be ready
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
 
-# waiting for kube-addon-manager to be ready
-JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl -n kube-system get pods -lcomponent=kube-addon-manager -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1;echo "waiting for kube-addon-manager to be available"; kubectl get pods --all-namespaces; done
-
 # waiting for kube-dns to be ready
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl -n kube-system get pods -lk8s-app=kube-dns -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1;echo "waiting for kube-dns to be available"; kubectl get pods --all-namespaces; done
 
-sudo ${MINIKUBE} addons enable ingress
+minikube addons enable ingress
 
 eval $(minikube docker-env)
