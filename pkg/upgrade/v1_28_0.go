a15b103c9b4f449143a037e186c1bdb8cde8cf5d
Pavol Loffay <p.loffay@gmail.com>
2021-10-18 10:36:07 +0200

    Fix
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- pkg/upgrade/v1_28_0.go
+++ pkg/upgrade/v1_28_0.go
@@ -2,7 +2,6 @@ package upgrade
 
 import (
 	"context"
-	"fmt"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -10,7 +9,6 @@ import (
 )
 
 func upgrade1_28_0(_ context.Context, _ client.Client, jaeger v1.Jaeger) (v1.Jaeger, error) {
-	fmt.Println("upgrading to jaeger 1.28")
 	// Set the SAR to empty to ignore normalization to a default SAR
 	// This workaround prevents a breaking change in already deployed Jaeger instances
 	if jaeger.Spec.Ingress.Openshift.SAR == nil {
