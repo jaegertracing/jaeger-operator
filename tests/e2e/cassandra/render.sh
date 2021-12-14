5c96565854b7b2b61d1d1ea7254de1387d2191e4
Israel Blancas <iblancas@redhat.com>
2021-12-14 21:51:01 +0100

    Fix issue in cassandra test
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

--- tests/e2e/cassandra/render.sh
+++ tests/e2e/cassandra/render.sh
@@ -7,10 +7,15 @@ cd $SUITE_DIR
 echo "Rendering templates for cassandra-smoke test"
 cd cassandra-smoke
 export CASSANDRA_INSTANCE_NAME=with-cassandra
+export JAEGER_NAME=with-cassandra
+export JAEGER_SERVICE=with-cassandra
+export JAEGER_OPERATION=smoketestoperation
 $GOMPLATE -f $TEMPLATES_DIR/cassandra-install.yaml.template -o ./00-install.yaml
 $GOMPLATE -f $TEMPLATES_DIR/cassandra-assert.yaml.template -o ./00-assert.yaml
 $GOMPLATE -f $TEMPLATES_DIR/cassandra-jaeger-install.yaml.template -o ./01-install.yaml
 $GOMPLATE -f $TEMPLATES_DIR/cassandra-jaeger-assert.yaml.template -o ./01-assert.yaml
+$GOMPLATE -f $TEMPLATES_DIR/smoke-test.yaml.template -o ./02-smoke-test.yaml
+$GOMPLATE -f $TEMPLATES_DIR/smoke-test-assert.yaml.template -o ./02-assert.yaml
 
 cd ../
 
