f5b4515abe7a9cb4c0f74b78e439fe04ff26305e
Megrez Lu <lujiajing1126@gmail.com>
2021-12-22 20:46:36 +0800

    move dir
    Signed-off-by: Megrez Lu <lujiajing1126@gmail.com>

--- tests/e2e/sidecard/render.sh
+++ tests/e2e/sidecard/render.sh
@@ -25,3 +25,15 @@ JOB_NUMBER=1 $GOMPLATE -f $TEMPLATES_DIR/assert-find-service.yaml.template -o ./
 export JAEGER_NAME=agent-as-sidecar2
 JOB_NUMBER=2 $GOMPLATE -f $TEMPLATES_DIR/find-service.yaml.template -o ./05-find-service-other-instance.yaml
 JOB_NUMBER=2 $GOMPLATE -f $TEMPLATES_DIR/assert-find-service.yaml.template -o ./05-assert.yaml
+
+cd ..
+
+echo "Rendering templates for sidecar-pod-namespace test"
+cd sidecar-pod-namespace
+export JAEGER_NAME=agent-as-sidecar
+export JAEGER_SERVICE=order
+JOB_NUMBER=1 $GOMPLATE -f $TEMPLATES_DIR/find-service.yaml.template -o ./02-find-service.yaml
+JOB_NUMBER=1 $GOMPLATE -f $TEMPLATES_DIR/assert-find-service.yaml.template -o ./02-assert.yaml
+export JAEGER_NAME=agent-as-sidecar2
+JOB_NUMBER=2 $GOMPLATE -f $TEMPLATES_DIR/find-service.yaml.template -o ./05-find-service-other-instance.yaml
+JOB_NUMBER=2 $GOMPLATE -f $TEMPLATES_DIR/assert-find-service.yaml.template -o ./05-assert.yaml
