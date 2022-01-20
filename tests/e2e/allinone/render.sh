89bbe0884fbbff42c1660119ae00c134d7a2d299
Israel Blancas <iblancas@redhat.com>
2022-01-20 21:25:30 +0100

    Fix issue with AiO after merge
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

--- tests/e2e/allinone/render.sh
+++ tests/e2e/allinone/render.sh
@@ -37,7 +37,6 @@ export TRACKING_ID=MyTrackingId
 # The URL is decided when the tests starts. So, the YAML file for the job is rendered after the test started
 if [ $IS_OPENSHIFT = true ]; then
     GET_URL_COMMAND="kubectl get routes -o=jsonpath='{.items[0].status.ingress[0].host}' -n \$NAMESPACE"
-    GET_URL_COMMAND="https://\$($GET_URL_COMMAND)"
 else
     GET_URL_COMMAND="echo http://all-in-one-with-ui-config-query:16686"
 fi
