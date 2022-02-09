4cc12b6dc8d8bbd081013eab4c48cb3ae92ea79c
Israel Blancas <iblancas@redhat.com>
2022-02-09 12:45:20 +0100

    Fix typo
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

--- tests/e2e/istio/render.sh
+++ tests/e2e/istio/render.sh
@@ -11,7 +11,7 @@ else
     render_find_service "$jaeger_name" "order" "00" "04"
 
     # One of the first steps of this test is enabling the Istio sidecar injection
-    # for the namespace. That means, each pod is started will have an Istio sidecard.
+    # for the namespace. That means, each pod is started will have an Istio sidecar.
     # A Job is not considered complete until all containers have stopped running.
     # Istio Sidecars run indefinitely. So, when a job is started, the sidecar will
     # stay forever there and the test will be marked as failed. Also, since the
