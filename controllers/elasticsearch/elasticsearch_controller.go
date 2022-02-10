1bd6cf794f9fcc4e45823a8054113a38e4b70bf7
Pavol Loffay <p.loffay@gmail.com>
2022-02-10 13:16:18 +0100

    Install ES controller only if ES Kind is installed
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- controllers/elasticsearch/elasticsearch_controller.go
+++ controllers/elasticsearch/elasticsearch_controller.go
@@ -2,8 +2,11 @@ package elasticsearch
 
 import (
 	"context"
+	"strings"
 
 	esv1 "github.com/openshift/elasticsearch-operator/apis/logging/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/discovery"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -31,7 +34,50 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&esv1.Elasticsearch{}).
-		Complete(r)
+	esKindInstalled, err := isOpenShiftElasticsearchController(mgr)
+	if err != nil {
+		return err
+	}
+	if esKindInstalled {
+		return ctrl.NewControllerManagedBy(mgr).
+			For(&esv1.Elasticsearch{}).
+			Complete(r)
+	}
+	return nil
+}
+
+const elasticsearchGroup = "logging.openshift.io"
+const elasticsearchKind = "Elasticsearch"
+
+func isOpenShiftElasticsearchController(mgr ctrl.Manager) (bool, error) {
+	dcl, err := discovery.NewDiscoveryClientForConfig(mgr.GetConfig())
+	if err != nil {
+		return false, err
+	}
+	var apiLists []*metav1.APIResourceList
+	groupList, err := dcl.ServerGroups()
+	if err != nil {
+		return false, err
+	}
+
+	for _, sg := range groupList.Groups {
+		if sg.Name == elasticsearchGroup {
+			groupAPIList, err := dcl.ServerResourcesForGroupVersion(sg.PreferredVersion.GroupVersion)
+			if err != nil {
+				return false, err
+			}
+			apiLists = append(apiLists, groupAPIList)
+		}
+	}
+
+	for _, r := range apiLists {
+		if strings.HasPrefix(r.GroupVersion, elasticsearchGroup) {
+			for _, api := range r.APIResources {
+				if api.Kind == elasticsearchKind {
+					return true, nil
+				}
+			}
+		}
+	}
+	return false, nil
 }
