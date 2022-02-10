dca86914420dda658d2358735e3544abda71b0d1
Pavol Loffay <p.loffay@gmail.com>
2022-02-10 16:37:40 +0100

    Reuse as much as possible
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- controllers/elasticsearch/elasticsearch_controller.go
+++ controllers/elasticsearch/elasticsearch_controller.go
@@ -2,14 +2,13 @@ package elasticsearch
 
 import (
 	"context"
-	"strings"
 
 	esv1 "github.com/openshift/elasticsearch-operator/apis/logging/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/discovery"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/jaegertracing/jaeger-operator/pkg/autodetect"
 	"github.com/jaegertracing/jaeger-operator/pkg/controller/elasticsearch"
 )
 
@@ -47,37 +46,15 @@ func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
 }
 
 const elasticsearchGroup = "logging.openshift.io"
-const elasticsearchKind = "Elasticsearch"
 
 func isOpenShiftElasticsearchController(mgr ctrl.Manager) (bool, error) {
 	dcl, err := discovery.NewDiscoveryClientForConfig(mgr.GetConfig())
 	if err != nil {
 		return false, err
 	}
-	var apiLists []*metav1.APIResourceList
-	groupList, err := dcl.ServerGroups()
+	apiLists, err := autodetect.AvailableAPIs(dcl, map[string]bool{elasticsearchGroup: true})
 	if err != nil {
 		return false, err
 	}
-
-	for _, sg := range groupList.Groups {
-		if sg.Name == elasticsearchGroup {
-			groupAPIList, err := dcl.ServerResourcesForGroupVersion(sg.PreferredVersion.GroupVersion)
-			if err != nil {
-				return false, err
-			}
-			apiLists = append(apiLists, groupAPIList)
-		}
-	}
-
-	for _, r := range apiLists {
-		if strings.HasPrefix(r.GroupVersion, elasticsearchGroup) {
-			for _, api := range r.APIResources {
-				if api.Kind == elasticsearchKind {
-					return true, nil
-				}
-			}
-		}
-	}
-	return false, nil
+	return autodetect.IsElasticsearchOperatorAvailable(apiLists), nil
 }
