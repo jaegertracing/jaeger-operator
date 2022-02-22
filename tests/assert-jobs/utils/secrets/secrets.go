81fdc96da3af226b4c522e3f4f0fa70dfb4e6a55
Israel Blancas <iblancas@redhat.com>
2022-02-22 01:46:06 +0100

    Port E2E KUTTL tests to OpenShift (#1715)
    * #1556: add support for OpenShift in E2E tests
    
    Signed-off-by: Israel Blancas <iblancas@redhat.com>
    
    * Apply changes requested in CR
    
    Signed-off-by: Israel Blancas <iblancas@redhat.com>
    
    * Apply changes requested in CR
    
    Signed-off-by: Israel Blancas <iblancas@redhat.com>
    
    * Fix lint warning
    
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

---
+++ tests/assert-jobs/utils/secrets/secrets.go
@@ -0,0 +1,20 @@
+package secrets
+
+import (
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/sirupsen/logrus"
+)
+
+// GetToken reads the token from the given path and returns it
+func GetToken(path string) string {
+	content, err := ioutil.ReadFile(filepath.Clean(path))
+
+	if err != nil {
+		logrus.Errorln("Something failed during reading the token:", err)
+		return ""
+	}
+
+	return string(content)
+}
