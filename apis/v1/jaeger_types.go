7762494d0f4fd095789411bc3b9e8985f9c61e0b
Jared Tan <jian.tan@daocloud.io>
2022-03-04 18:27:57 +0800

    add METRICS_STORAGE_TYPE for metrics query (#1755)
    * finish
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * revert CHANGELOG.md
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * fix bundle.
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>
    
    * fix jaeger image.
    
    Signed-off-by: jian.tan <jian.tan@daocloud.io>

--- apis/v1/jaeger_types.go
+++ apis/v1/jaeger_types.go
@@ -232,6 +232,9 @@ type JaegerQuerySpec struct {
 	Options Options `json:"options,omitempty"`
 
 	// +optional
+	MetricsStorage JaegerMetricsStorageSpec `json:"metricsStorage,omitempty"`
+
+	// +optional
 	JaegerCommonSpec `json:",inline,omitempty"`
 
 	// +optional
@@ -350,6 +353,9 @@ type JaegerAllInOneSpec struct {
 	// +optional
 	// +kubebuilder:pruning:PreserveUnknownFields
 	Config FreeForm `json:"config,omitempty"`
+
+	// +optional
+	MetricsStorage JaegerMetricsStorageSpec `json:"metricsStorage,omitempty"`
 
 	// +optional
 	JaegerCommonSpec `json:",inline,omitempty"`
@@ -507,6 +513,12 @@ type JaegerStorageSpec struct {
 
 	// +optional
 	GRPCPlugin GRPCPluginSpec `json:"grpcPlugin,omitempty"`
+}
+
+// JaegerMetricsStorageSpec defines the Metrics storage options to be used for the query and collector.
+type JaegerMetricsStorageSpec struct {
+	// +optional
+	Type JaegerStorageType `json:"type,omitempty"`
 }
 
 // ElasticsearchSpec represents the ES configuration options that we pass down to the OpenShift Elasticsearch operator.
