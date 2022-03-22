2b6954a0e0ae0e49aac88df9cfcb933a69e3afe7
Israel Blancas <iblancas@redhat.com>
2022-03-22 14:09:46 +0100

    Fix path to KUTTL installation
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

--- hack/run-e2e-tests.sh
+++ hack/run-e2e-tests.sh
@@ -21,7 +21,7 @@ root_dir=$current_dir/../
 reports_dir=$root_dir/reports
 
 # Ensure KUTTL is installed
-$current_dir/install/install-kuttl.sh
+$root_dir/install/install-kuttl.sh
 export KUTTL=$current_dir/bin/kubectl-kuttl
 
 mkdir -p $reports_dir
