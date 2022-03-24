9ecebbbeaf76a0cc815724aa61c9601179952425
Benedikt Bongartz <bongartz@klimlive.de>
2022-03-24 13:46:28 +0100

    fix sidecar tests
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>

--- tests/e2e/sidecar/render.sh
+++ tests/e2e/sidecar/render.sh
@@ -13,15 +13,9 @@ render_find_service "agent-as-sidecar2" "$jaeger_service_name" "02" "05"
 start_test "sidecar-deployment"
 render_install_vertx "01"
 
+start_test "sidecar-pod"
 
 start_test "sidecar-namespace"
-jaeger_service_name="order"
-render_install_vertx "01"
-render_find_service "agent-as-sidecar" "$jaeger_service_name" "01" "02"
-render_find_service "agent-as-sidecar2" "$jaeger_service_name" "02" "05"
-
-
-start_test "sidecar-pod-namespace"
 jaeger_service_name="order"
 render_install_vertx "01"
 render_find_service "agent-as-sidecar" "$jaeger_service_name" "01" "02"
