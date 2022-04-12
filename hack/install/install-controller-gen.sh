3e456b701edb456b10a7da724fa52acedc57a241
Israel Blancas <iblancas@redhat.com>
2022-04-12 23:14:50 +0200

    Install controller-gen only when it is needed (#1847)
    Signed-off-by: Israel Blancas <iblancas@redhat.com>
    
    Co-authored-by: Ruben Vargas <ruben.vp8510@gmail.com>

---
+++ hack/install/install-controller-gen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+VERSION="0.8.0"
+
+echo "Installing controller-gen"
+
+current_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $current_dir/install-utils.sh
+
+PROGRAM="controller-gen"
+
+create_bin
+
+export GOBIN=$BIN
+
+check_tool "$BIN/$PROGRAM" "$VERSION" "--version"
+
+retry "go install sigs.k8s.io/controller-tools/cmd/controller-gen@v${VERSION}"
