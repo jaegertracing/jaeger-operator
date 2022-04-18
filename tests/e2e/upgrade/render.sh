4743678e87c40337a30febbeb5b1627ced94b122
Israel Blancas <iblancas@redhat.com>
2022-04-18 17:13:34 +0200

    Fix upgrade test
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

--- tests/e2e/upgrade/render.sh
+++ tests/e2e/upgrade/render.sh
@@ -9,5 +9,5 @@ else
     start_test "upgrade"
     $GOMPLATE -f ./deployment-assert.yaml.template -o ./00-assert.yaml
     JAEGER_VERSION=$($ROOT_DIR/.ci/get_test_upgrade_version.sh $JAEGER_VERSION) $GOMPLATE -f ./deployment-assert.yaml.template -o ./01-assert.yaml
-    sed "s~$IMG~$OPERATOR_IMAGE_NEXT~gi" $ROOT_DIR/tests/_build/manifests/01-jaeger-operator.yaml > ./operator-upgrade.yaml
+    $GOMPLATE -f ./01-replace.yaml.template -o ./01-replace.yaml
 fi
