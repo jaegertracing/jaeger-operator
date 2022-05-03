c68466a4efa3b1d4c92502625e227eb408b4cb27
Israel Blancas <iblancas@redhat.com>
2022-05-03 20:28:22 +0200

    Create a common script for variables
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

---
+++ hack/common.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Folders
+export TEST_DIR=$ROOT_DIR/tests
+export TEMPLATES_DIR=$TEST_DIR/templates
+export EXAMPLES_DIR=$ROOT_DIR/examples
+
+# Tools
+export GOMPLATE=$ROOT_DIR/bin/gomplate
+export YQ=$ROOT_DIR/bin/yq
