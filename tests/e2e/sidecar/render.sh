8853031c28b07efbfad7965874ecddbb8a70e346
Israel Blancas <iblancas@redhat.com>
2022-05-26 11:38:35 +0200

    Fix function call
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

--- tests/e2e/sidecar/render.sh
+++ tests/e2e/sidecar/render.sh
@@ -18,7 +18,7 @@ start_test "sidecar-namespace"
 render_install_vertx "01"
 # After removing the first Jaeger instance, we should be able to continue
 # receiving spans in the second one
-render_find_service "agent-as-sidecar" "$jaeger_service_name" "00" "03"
+render_find_service "agent-as-sidecar" "allInOne" "$jaeger_service_name" "00" "03"
 # Check Jaeger is receiving spans
 render_find_service "agent-as-sidecar2" "allInOne" "$jaeger_service_name" "01" "06"
 
