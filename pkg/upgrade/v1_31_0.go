eaa09b33ec6c23a54dfeda335d796fc12eb4ec4c
Jeeva Kandasamy <jkandasa@gmail.com>
2022-05-30 16:21:33 +0530

    upgrade: ignore not found error in es instance deletion
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- pkg/upgrade/v1_31_0.go
+++ pkg/upgrade/v1_31_0.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 
 	esv1 "github.com/openshift/elasticsearch-operator/apis/logging/v1"
+	log "github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -25,6 +27,15 @@ func upgrade1_31_0(ctx context.Context, c client.Client, jaeger v1.Jaeger) (v1.J
 		}
 		err := c.Delete(ctx, &es)
 		if err != nil {
+			// ignore the error if the requested Elasticsearch resource is not available to delete
+			if errors.IsNotFound(err) {
+				jaeger.Logger().WithFields(log.Fields{
+					"instance":  jaeger.Name,
+					"namespace": jaeger.Namespace,
+					"current":   jaeger.Status.Version,
+				}).Debug("Requested 'elasticsearch' instance is not available to delete")
+				return jaeger, nil
+			}
 			return jaeger, fmt.Errorf("failed to delete Elasticsearch, deletion is needed for certificate migration to Elasticsearch operator: %v", err)
 		}
 	}
