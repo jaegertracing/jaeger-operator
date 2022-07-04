efd6b2655707795725181d352cf64438b43f6a2e
Israel Blancas <iblancas@redhat.com>
2022-07-04 23:37:07 +0200

    Change the parameters for the autoscale test. Fix streaming-autoscale test
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

--- tests/e2e/miscellaneous/render.sh
+++ tests/e2e/miscellaneous/render.sh
@@ -41,37 +41,12 @@ $YQ e -i '.spec.collector.resources.requests.memory="300m"' 01-install.yaml
 # Enable autoscale
 $YQ e -i '.spec.collector.autoscale=true' 01-install.yaml
 $YQ e -i '.spec.collector.minReplicas=1' 01-install.yaml
-$YQ e -i '.spec.collector.maxReplicas=5' 01-install.yaml
+$YQ e -i '.spec.collector.maxReplicas=3' 01-install.yaml
 
 # Deploy Tracegen instance to generate load in the Jaeger collector
-render_install_tracegen "$jaeger_name" "3" "02"
+render_install_tracegen "$jaeger_name" "02"
 
 
-if [ $IS_OPENSHIFT = true ]; then
-    start_test "collector-autoscale"
-
-    jaeger_name="simple-prod"
-    ELASTICSEARCH_NODECOUNT="1"
-    render_install_jaeger "$jaeger_name" "production" "00"
-
-    $GOMPLATE -f $TEMPLATES_DIR/assert-tracegen.yaml.template -o ./01-assert.yaml
-
-    # Change the resource limits for the autoprovisioned deployment
-    $YQ e -i '.spec.collector.resources.requests.memory="20Mi"' 00-install.yaml
-    $YQ e -i '.spec.collector.resources.requests.memory="100m"' 00-install.yaml
-
-    # Enable autoscale
-    $YQ e -i '.spec.collector.autoscale=true' 00-install.yaml
-    $YQ e -i '.spec.collector.minReplicas=1' 00-install.yaml
-    $YQ e -i '.spec.collector.maxReplicas=5' 00-install.yaml
-
-    # Deploy a Tracegen instance to generate load in the Jaeger collector
-    cp $EXAMPLES_DIR/tracegen.yaml ./01-install.yaml
-
-
-else
-    skip_test "collector-autoscale" "Test only supported in OpenShift"
-fi
 
 # Helper function to generate the same tests multiple times but with different
 # reporting protocols
