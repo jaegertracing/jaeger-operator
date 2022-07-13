12f68e1f10b6451beb5f0a04aec60cb9b1af2764
Israel Blancas <iblancas@redhat.com>
2022-07-13 12:37:23 +0200

    Disable unsupported test in OpenShift and add extra time for the SA creation
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

--- tests/e2e/examples/render.sh
+++ tests/e2e/examples/render.sh
@@ -17,13 +17,14 @@ render_install_example "$example_name" "01"
 render_smoke_test_example "$example_name" "02"
 
 
-if [ $IS_OPENSHIFT != true ]; then
+if [ $IS_OPENSHIFT = true ]; then
+    skip_test "examples-all-in-one-with-options" "This test is not supported in OpenShift"
+else
     start_test "examples-all-in-one-with-options"
     example_name="all-in-one-with-options"
     render_install_example "$example_name" "00"
     render_smoke_test_example "$example_name" "01"
     sed -i "s~16686~16686/jaeger~gi" ./01-smoke-test.yaml
-    sed -i "s~https://my-jaeger-query~https://my-jaeger-query/jaeger~gi" ./01-smoke-test.yaml
 fi
 
 
@@ -104,6 +105,7 @@ export example_name="with-sampling"
 render_install_cassandra "00"
 render_install_example "$example_name" "01"
 render_smoke_test_example "$example_name" "02"
+
 
 ###############################################################################
 # OpenShift examples ##########################################################
