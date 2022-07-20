4bc9882f6e4c2aa8c04bbcd6ccd959025f37cb8e
Israel Blancas <iblancas@redhat.com>
2022-07-20 19:15:14 +0200

    Increase robustness of cmctl download
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

---
+++ hack/install/install-cmctl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+VERSION=$1
+
+echo "Installing cmctl"
+
+current_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $current_dir/install-utils.sh
+
+PROGRAM="cmctl"
+
+create_bin
+
+url="https://github.com/jetstack/cert-manager/releases/download/v${VERSION}/cmctl-$(go env GOOS)-$(go env GOARCH).tar.gz"
+
+
+tar_file="/tmp/cmctl.tar.gz"
+retry "curl -sLo $tar_file $url"
+tar -xzf $tar_file -C /tmp/
+
+cp /tmp/$PROGRAM $BIN/
+chmod +x $BIN/$PROGRAM
+
+export PATH=$PATH:$BIN
