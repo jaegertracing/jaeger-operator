21a955265be3ddca2dfca7145994453b7b956b7f
Israel Blancas <iblancas@redhat.com>
2022-07-21 12:03:31 +0200

    Avoid reevaluation of the ASSERT_IMG Make variable (#2007)
    * Avoid regeneration of the ASSERT_IMG value
    
    Signed-off-by: Israel Blancas <iblancas@redhat.com>
    
    * Fix missing quotes
    
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

---
+++ tests/build-utils/build-assert-e2e-img.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Get the name to use for the E2E assert job
+#
+set -xe
+
+image_name=$1
+timestamp_file=$2
+docker build -t "$image_name" -f Dockerfile.asserts . $DOCKER_BUILD_OPTIONS
+echo "$image_name" > "$timestamp_file"
