1c77e57a35ff2fcf1b9583eba49ea388b722ef75
Israel Blancas <iblancasa@gmail.com>
2022-08-17 13:35:29 +0200

    Fix log call after merge
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>

--- pkg/strategy/streaming.go
+++ pkg/strategy/streaming.go
@@ -76,7 +76,9 @@ func newStreamingStrategy(ctx context.Context, jaeger *v1.Jaeger) S {
 	// we provision a Kafka when no brokers have been set, or, when we are not in the first run,
 	// when we know we've been the ones placing the broker information in the configuration
 	if (!pfound && !cfound) || provisioned {
-		jaeger.Logger().Debug("Kafka auto provisioning is enabled. A Kafka cluster will be deployed if it does not exist.")
+		jaeger.Logger().V(-1).Info(
+			"Kafka auto provisioning is enabled. A Kafka cluster will be deployed if it does not exist.",
+		)
 		manifest = autoProvisionKafka(ctx, jaeger, manifest)
 	}
 
