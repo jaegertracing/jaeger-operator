3aa38b51533b9db1b9afd6e4de78da18902b884b
Israel Blancas <iblancasa@gmail.com>
2022-08-25 17:41:09 +0200

    Fix some unit tests
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>

--- pkg/autodetect/main_test.go
+++ pkg/autodetect/main_test.go
@@ -457,6 +457,36 @@ func TestAutoDetectCronJobsVersion(t *testing.T) {
 	}
 }
 
+func TestAutoDetectAutoscalingVersion(t *testing.T) {
+	apiGroupVersions := []string{v1.FlagAutoscalingVersionV2, v1.FlagAutoscalingVersionV2Beta2}
+	for _, apiGroup := range apiGroupVersions {
+		dcl := &fakeDiscoveryClient{}
+		cl := fake.NewFakeClient() // nolint:staticcheck
+		b := WithClients(cl, dcl, cl)
+		dcl.ServerGroupsFunc = func() (apiGroupList *metav1.APIGroupList, err error) {
+			return &metav1.APIGroupList{Groups: []metav1.APIGroup{{
+				Name:     apiGroup,
+				Versions: []metav1.GroupVersionForDiscovery{{Version: apiGroup}},
+			}}}, nil
+		}
+
+		dcl.ServerResourcesForGroupVersionFunc = func(requestedApiVersion string) (apiGroupList *metav1.APIResourceList, err error) {
+			if requestedApiVersion == apiGroup {
+				apiResourceList := &metav1.APIResourceList{GroupVersion: apiGroup, APIResources: []metav1.APIResource{{Name: "autoscaling"}}}
+				return apiResourceList, nil
+			}
+			return &metav1.APIResourceList{}, nil
+		}
+
+		// test
+		b.autoDetectCapabilities()
+
+		// verify
+		assert.Equal(t, apiGroup, viper.GetString(v1.FlagAutoscalingVersion))
+		fmt.Printf("Test finished on [%s]\n", apiGroup)
+	}
+}
+
 func TestSkipAuthDelegatorNonOpenShift(t *testing.T) {
 	// prepare
 	viper.Set("platform", v1.FlagPlatformKubernetes)
--- pkg/controller/jaeger/horizontalpodautoscaler_test.go
+++ pkg/controller/jaeger/horizontalpodautoscaler_test.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"testing"
 
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
-	autoscalingv2beta2 "k8s.io/api/autoscaling/v2beta2"
+	autoscalingv2 "k8s.io/api/autoscaling/v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -14,6 +15,10 @@ import (
 	v1 "github.com/jaegertracing/jaeger-operator/apis/v1"
 	"github.com/jaegertracing/jaeger-operator/pkg/strategy"
 )
+
+func init() {
+	viper.SetDefault(v1.FlagCronJobsVersion, v1.FlagAutoscalingVersionV2)
+}
 
 func TestHorizontalPodAutoscalerCreate(t *testing.T) {
 	// prepare
@@ -32,12 +37,14 @@ func TestHorizontalPodAutoscalerCreate(t *testing.T) {
 
 	r, cl := getReconciler(objs)
 	r.strategyChooser = func(ctx context.Context, jaeger *v1.Jaeger) strategy.S {
-		s := strategy.New().WithHorizontalPodAutoscaler([]autoscalingv2beta2.HorizontalPodAutoscaler{{
+		hpa := &autoscalingv2.HorizontalPodAutoscaler{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      nsn.Name,
 				Namespace: nsn.Namespace,
 			},
-		}})
+		}
+		var autoscaler runtime.Object = hpa
+		s := strategy.New().WithHorizontalPodAutoscaler([]runtime.Object{autoscaler})
 		return s
 	}
 
@@ -48,7 +55,7 @@ func TestHorizontalPodAutoscalerCreate(t *testing.T) {
 	assert.NoError(t, err)
 	assert.False(t, res.Requeue, "We don't requeue for now")
 
-	persisted := &autoscalingv2beta2.HorizontalPodAutoscaler{}
+	persisted := &autoscalingv2.HorizontalPodAutoscaler{}
 	persistedName := types.NamespacedName{
 		Name:      nsn.Name,
 		Namespace: nsn.Namespace,
@@ -65,7 +72,7 @@ func TestHorizontalPodAutoscalerUpdate(t *testing.T) {
 		Namespace: "tenant1",
 	}
 
-	orig := autoscalingv2beta2.HorizontalPodAutoscaler{}
+	orig := autoscalingv2.HorizontalPodAutoscaler{}
 	orig.Name = nsn.Name
 	orig.Namespace = nsn.Namespace
 	orig.Annotations = map[string]string{"key": "value"}
@@ -81,12 +88,14 @@ func TestHorizontalPodAutoscalerUpdate(t *testing.T) {
 
 	r, cl := getReconciler(objs)
 	r.strategyChooser = func(ctx context.Context, jaeger *v1.Jaeger) strategy.S {
-		depUpdated := autoscalingv2beta2.HorizontalPodAutoscaler{}
+		depUpdated := autoscalingv2.HorizontalPodAutoscaler{}
 		depUpdated.Name = orig.Name
 		depUpdated.Namespace = orig.Namespace
 		depUpdated.Annotations = map[string]string{"key": "new-value"}
 
-		s := strategy.New().WithHorizontalPodAutoscaler([]autoscalingv2beta2.HorizontalPodAutoscaler{depUpdated})
+		var hpa runtime.Object = &depUpdated
+
+		s := strategy.New().WithHorizontalPodAutoscaler([]runtime.Object{hpa})
 		return s
 	}
 
@@ -95,7 +104,7 @@ func TestHorizontalPodAutoscalerUpdate(t *testing.T) {
 	assert.NoError(t, err)
 
 	// verify
-	persisted := &autoscalingv2beta2.HorizontalPodAutoscaler{}
+	persisted := &autoscalingv2.HorizontalPodAutoscaler{}
 	persistedName := types.NamespacedName{
 		Name:      orig.Name,
 		Namespace: orig.Namespace,
@@ -111,7 +120,7 @@ func TestHorizontalPodAutoscalerDelete(t *testing.T) {
 		Name: "TestHorizontalPodAutoscalerDelete",
 	}
 
-	orig := autoscalingv2beta2.HorizontalPodAutoscaler{}
+	orig := autoscalingv2.HorizontalPodAutoscaler{}
 	orig.Name = nsn.Name
 	orig.Labels = map[string]string{
 		"app.kubernetes.io/instance":   orig.Name,
@@ -133,7 +142,7 @@ func TestHorizontalPodAutoscalerDelete(t *testing.T) {
 	assert.NoError(t, err)
 
 	// verify
-	persisted := &autoscalingv2beta2.HorizontalPodAutoscaler{}
+	persisted := &autoscalingv2.HorizontalPodAutoscaler{}
 	persistedName := types.NamespacedName{
 		Name:      orig.Name,
 		Namespace: orig.Namespace,
@@ -157,7 +166,7 @@ func TestHorizontalPodAutoscalerCreateExistingNameInAnotherNamespace(t *testing.
 	objs := []runtime.Object{
 		v1.NewJaeger(nsn),
 		v1.NewJaeger(nsnExisting),
-		&autoscalingv2beta2.HorizontalPodAutoscaler{
+		&autoscalingv2.HorizontalPodAutoscaler{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      nsnExisting.Name,
 				Namespace: nsnExisting.Namespace,
@@ -171,12 +180,13 @@ func TestHorizontalPodAutoscalerCreateExistingNameInAnotherNamespace(t *testing.
 
 	r, cl := getReconciler(objs)
 	r.strategyChooser = func(ctx context.Context, jaeger *v1.Jaeger) strategy.S {
-		s := strategy.New().WithHorizontalPodAutoscaler([]autoscalingv2beta2.HorizontalPodAutoscaler{{
+		var hpa runtime.Object = &autoscalingv2.HorizontalPodAutoscaler{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      nsn.Name,
 				Namespace: nsn.Namespace,
 			},
-		}})
+		}
+		s := strategy.New().WithHorizontalPodAutoscaler([]runtime.Object{hpa})
 		return s
 	}
 
@@ -187,13 +197,13 @@ func TestHorizontalPodAutoscalerCreateExistingNameInAnotherNamespace(t *testing.
 	assert.NoError(t, err)
 	assert.False(t, res.Requeue, "We don't requeue for now")
 
-	persisted := &autoscalingv2beta2.HorizontalPodAutoscaler{}
+	persisted := &autoscalingv2.HorizontalPodAutoscaler{}
 	err = cl.Get(context.Background(), nsn, persisted)
 	assert.NoError(t, err)
 	assert.Equal(t, nsn.Name, persisted.Name)
 	assert.Equal(t, nsn.Namespace, persisted.Namespace)
 
-	persistedExisting := &autoscalingv2beta2.HorizontalPodAutoscaler{}
+	persistedExisting := &autoscalingv2.HorizontalPodAutoscaler{}
 	err = cl.Get(context.Background(), nsnExisting, persistedExisting)
 	assert.NoError(t, err)
 	assert.Equal(t, nsnExisting.Name, persistedExisting.Name)
