04123a85dd37fae60842b3cde1c2c246cc0fd10b
Giau. Tran Minh <giau.tmg@gmail.com>
2022-09-16 22:14:39 +0700

    feat: added pathType to ingress
    Signed-off-by: Giau. Tran Minh <hello@giautm.dev>

--- pkg/ingress/query_test.go
+++ pkg/ingress/query_test.go
@@ -7,6 +7,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 
 	v1 "github.com/jaegertracing/jaeger-operator/apis/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 )
 
 func TestQueryIngress(t *testing.T) {
@@ -140,6 +141,29 @@ func TestQueryIngressWithHosts(t *testing.T) {
 
 	assert.Len(t, dep.Spec.Rules[0].HTTP.Paths, 1)
 	assert.Empty(t, dep.Spec.Rules[0].HTTP.Paths[0].Path)
+	assert.Equal(t, networkingv1.PathType("ImplementationSpecific"), *dep.Spec.Rules[0].HTTP.Paths[0].PathType)
+	assert.Equal(t, "test-host-1", dep.Spec.Rules[0].Host)
+	assert.NotNil(t, dep.Spec.Rules[0].HTTP.Paths[0].Backend)
+}
+
+func TestQueryIngressWithPathType(t *testing.T) {
+	enabled := true
+	jaeger := v1.NewJaeger(types.NamespacedName{Name: "TestQueryIngressWithHosts"})
+	jaeger.Spec.Ingress.Enabled = &enabled
+	jaeger.Spec.Ingress.PathType = networkingv1.PathType("Prefix")
+	jaeger.Spec.Ingress.Hosts = []string{"test-host-1"}
+
+	ingress := NewQueryIngress(jaeger)
+
+	dep := ingress.Get()
+
+	assert.NotNil(t, dep)
+	assert.Nil(t, dep.Spec.DefaultBackend)
+	assert.Len(t, dep.Spec.Rules, 1)
+
+	assert.Len(t, dep.Spec.Rules[0].HTTP.Paths, 1)
+	assert.Empty(t, dep.Spec.Rules[0].HTTP.Paths[0].Path)
+	assert.Equal(t, networkingv1.PathType("Prefix"), *dep.Spec.Rules[0].HTTP.Paths[0].PathType)
 	assert.Equal(t, "test-host-1", dep.Spec.Rules[0].Host)
 	assert.NotNil(t, dep.Spec.Rules[0].HTTP.Paths[0].Backend)
 }
