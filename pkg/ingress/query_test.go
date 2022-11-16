226ea05f649a17a9fa4f62d41341c573cad25410
Kevin Earls <kearls@redhat.com>
2022-11-16 11:46:08 -0500

    Remove redundant test
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- pkg/ingress/query_test.go
+++ pkg/ingress/query_test.go
@@ -85,27 +85,6 @@ func TestIngressWithPath(t *testing.T) {
 	}
 }
 
-func TestQueryIngressQueryBasePathWithStreaming(t *testing.T) {
-	enabled := true
-	name := "TestQueryIngressQueryBasePath"
-	jaeger := v1.NewJaeger(types.NamespacedName{Name: name})
-	jaeger.Spec.Ingress.Enabled = &enabled
-	jaeger.Spec.Strategy = v1.DeploymentStrategyStreaming
-	jaeger.Spec.Query.Options = v1.NewOptions(map[string]interface{}{"query.base-path": "/jaeger-streaming"})
-	ingress := NewQueryIngress(jaeger)
-
-	dep := ingress.Get()
-
-	assert.NotNil(t, dep)
-	assert.Nil(t, dep.Spec.DefaultBackend)
-	assert.Len(t, dep.Spec.Rules, 1)
-
-	assert.Len(t, dep.Spec.Rules[0].HTTP.Paths, 1)
-	assert.Equal(t, "/jaeger-streaming", dep.Spec.Rules[0].HTTP.Paths[0].Path)
-	assert.Empty(t, dep.Spec.Rules[0].Host)
-	assert.NotNil(t, dep.Spec.Rules[0].HTTP.Paths[0].Backend)
-}
-
 func TestQueryIngressAnnotations(t *testing.T) {
 	jaeger := v1.NewJaeger(types.NamespacedName{Name: "TestQueryIngressAnnotations"})
 	jaeger.Spec.Annotations = map[string]string{
