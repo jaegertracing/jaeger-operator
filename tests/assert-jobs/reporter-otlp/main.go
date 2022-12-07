ed035369f915757e84101b5afbe31174f0524967
Vicente Olmedo <vicente.olmedo@gmail.com>
2022-12-07 08:30:54 +0100

    Update otel packages to v1.10.0+v0.32.1 (#2084)
    * Update otel packages to v1.10.0
    
    Signed-off-by: Vicente Olmedo <vicente.olmedo@gmail.com>
    
    * Properly use OTEL_EXPORTER_OTLP_ENDPOINT environment variable
    
    Signed-off-by: Vicente Olmedo <vicente.olmedo@gmail.com>
    
    Signed-off-by: Vicente Olmedo <vicente.olmedo@gmail.com>

--- tests/assert-jobs/reporter-otlp/main.go
+++ tests/assert-jobs/reporter-otlp/main.go
@@ -16,16 +16,13 @@ import (
 	"github.com/spf13/viper"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp"
-	"go.opentelemetry.io/otel/exporters/otlp/otlpgrpc"
-	"go.opentelemetry.io/otel/exporters/otlp/otlphttp"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/propagation"
-	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
-	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
-	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	"go.opentelemetry.io/otel/semconv"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
 )
 
 const (
@@ -64,102 +61,44 @@ func initCmd() error {
 	return err
 }
 
-// Get the endpoint where the collector is listening
-func getCollector() string {
-	reportingProtocol := viper.GetString(flagReportingProtocol)
-	jaegerEndpoint := viper.GetString(otlpExporterEndpoint)
-
-	var endpoint string
-	switch reportingProtocol {
-	case "grpc":
-		endpoint = fmt.Sprintf("%s:4317", jaegerEndpoint)
-	case "http":
-		endpoint = fmt.Sprintf("%s:4318", jaegerEndpoint)
-	default:
-		logrus.Fatalln("Reporting protocol", reportingProtocol, "not recognized")
-	}
-	return endpoint
-}
-
 // Initializes an OTLP exporter and configure the traces provider
 func initProvider(serviceName string) func() {
 	logrus.Debugln("Initializing the OTLP exporter")
 	ctx := context.Background()
 
-	collector := getCollector()
-
 	reportingProtocol := viper.GetString(flagReportingProtocol)
 
 	logrus.Debugln("Using", reportingProtocol, "to report the traces")
 
-	var driver otlp.ProtocolDriver
-
+	var exp *otlptrace.Exporter
+	var err error
 	switch reportingProtocol {
 	case "grpc":
-		driver = otlpgrpc.NewDriver(
-			otlpgrpc.WithInsecure(),
-			otlpgrpc.WithEndpoint(collector),
-			otlpgrpc.WithDialOption(grpc.WithBlock()),
-		)
+		exp, err = otlptracegrpc.New(ctx, otlptracegrpc.WithDialOption(grpc.WithBlock()))
 	case "http":
-		driver = otlphttp.NewDriver(
-			otlphttp.WithInsecure(),
-			otlphttp.WithEndpoint(collector),
-		)
+		exp, err = otlptracehttp.New(ctx)
 	default:
 		logrus.Fatalln("Reporting protocol", reportingProtocol, "not recognized")
 	}
 
-	exp, err := otlp.NewExporter(ctx, driver)
 	if err != nil {
-		logrus.Fatalln("error creating the exporter", err)
+		logrus.Fatalln("error creating", reportingProtocol, "exporter", err)
 	}
 
-	res, err := resource.New(ctx,
-		resource.WithAttributes(
-			semconv.ServiceNameKey.String(serviceName),
-		),
-	)
-	if err != nil {
-		logrus.Fatalln("error creating the resource", err)
-	}
-
-	bsp := sdktrace.NewBatchSpanProcessor(exp)
 	tracerProvider := sdktrace.NewTracerProvider(
 		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithResource(res),
-		sdktrace.WithSpanProcessor(bsp),
-	)
-
-	cont := controller.New(
-		processor.New(
-			simple.NewWithExactDistribution(),
-			exp,
-		),
-		controller.WithExporter(exp),
-		controller.WithCollectPeriod(2*time.Second),
+		sdktrace.WithResource(resource.NewWithAttributes(semconv.SchemaURL, semconv.ServiceNameKey.String(serviceName))),
+		sdktrace.WithSpanProcessor(sdktrace.NewBatchSpanProcessor(exp)),
 	)
 
 	otel.SetTextMapPropagator(propagation.TraceContext{})
 	otel.SetTracerProvider(tracerProvider)
 
-	err = cont.Start(context.Background())
-	if err != nil {
-		logrus.Fatalln("error while starting the controller", err)
-	}
-
 	// This function should be called when the tracing features will not be
 	// used anymore
 	return func() {
-		err1 := cont.Stop(context.Background())
-		err2 := tracerProvider.Shutdown(ctx)
-
-		// The errors are checked later to try to run all the "closing" tasks
-		if err1 != nil {
-			logrus.Fatalln("failed while stopping the controller", err1)
-		}
-		if err2 != nil {
-			logrus.Fatalln("failed to shutting down the tracer provider", err2)
+		if err := tracerProvider.Shutdown(ctx); err != nil {
+			logrus.Fatalln("failed to shutting down the tracer provider", err)
 		}
 	}
 }
@@ -180,10 +119,9 @@ func generateSubSpans(ctx context.Context, depth int) {
 }
 
 // Generate some traces
-// jaegerEnpoint: where Jaeger endpoint is located
 // serviceName: service to use for the span
 // operationName: operation described in the span
-func generateTraces(jaegerEndpoint string, serviceName string, operationName string) {
+func generateTraces(serviceName string, operationName string) {
 	logrus.Debugln("Trying to generate traces")
 	shutdown := initProvider(serviceName)
 	defer shutdown()
@@ -221,8 +159,8 @@ func main() {
 		// To avoid creating all the files for gRPC, we just wait some time
 		time.Sleep(time.Second * 5)
 	case "http":
-		err = utils.WaitUntilRestAPIAvailable(fmt.Sprintf("http://%s", getCollector()))
-		if err != nil {
+		jaegerEndpoint := viper.GetString(otlpExporterEndpoint)
+		if err := utils.WaitUntilRestAPIAvailable(jaegerEndpoint); err != nil {
 			logrus.Fatalln(err)
 		}
 	default:
@@ -230,7 +168,6 @@ func main() {
 	}
 
 	generateTraces(
-		viper.GetString(otlpExporterEndpoint),
 		viper.GetString(flagJaegerServiceName),
 		viper.GetString(flagJaegerOperationName),
 	)
