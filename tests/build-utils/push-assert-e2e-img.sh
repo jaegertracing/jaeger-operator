16965158579b5dfcc1ab45953ec04f3bff605041
Jeeva Kandasamy <jkandasa@gmail.com>
2022-12-21 15:23:27 +0530

    conditional buildx support for multi arch
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- tests/build-utils/push-assert-e2e-img.sh
+++ tests/build-utils/push-assert-e2e-img.sh
@@ -11,4 +11,14 @@ ASSERT_IMG="$("$current_dir"/get-assert-e2e-img.sh)"
 
 if [ "$USE_KIND_CLUSTER" = true ]; then
     "$root_dir"/hack/load-kind-image.sh "$ASSERT_IMG"
+elif [ "$MULTI_ARCH_ASSERT_IMG" = false ]; then
+    echo "Pushing the E2E Test asserts Docker image to the remote registry"
+
+    # Check if the image is in the container registry
+    manifest=$(docker manifest inspect "$ASSERT_IMG" 2>/dev/null || true)
+    if [ -z "$manifest" ]; then
+        docker push "$ASSERT_IMG"
+    else
+        echo "The image $ASSERT_IMG is in the registry. Not pushing"
+    fi
 fi
