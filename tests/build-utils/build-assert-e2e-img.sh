334c9332d1d68a07756308291ca5f58b34aa5f23
Jeeva Kandasamy <jkandasa@gmail.com>
2022-12-21 14:11:56 +0530

    support e2e tests on multi arch environment
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- tests/build-utils/build-assert-e2e-img.sh
+++ tests/build-utils/build-assert-e2e-img.sh
@@ -6,5 +6,18 @@ set -xe
 
 image_name=$1
 timestamp_file=$2
-docker build -t "$image_name" -f Dockerfile.asserts . $DOCKER_BUILD_OPTIONS
+
+# check if the image is in the container registry
+manifest=$(docker manifest inspect "$image_name" 2>/dev/null || true)
+if [ -z "$manifest" ]; then
+    echo "the e2e test asserts container image is not available in the remote registry. locally building and pushing into remote registry"
+    docker buildx build --load \
+        --progress=plain \
+        --platform ${PLATFORMS} \
+        --file Dockerfile.asserts \
+        --tag ${image_name} .
+else
+    echo "the e2e test asserts container image is available in the remote registry. pulling into local environment"
+    docker pull "$image_name"
+fi
 echo "$image_name" > "$timestamp_file"
