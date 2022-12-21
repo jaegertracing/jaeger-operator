6c1c4fa59b9b28b1173821c2a307d10eea4ac475
Jeeva Kandasamy <jkandasa@gmail.com>
2022-12-21 15:28:11 +0530

    verify remote registry only on multi arch image
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- tests/build-utils/build-assert-e2e-img.sh
+++ tests/build-utils/build-assert-e2e-img.sh
@@ -7,21 +7,22 @@ set -xe
 image_name=$1
 timestamp_file=$2
 
-# check if the image is in the container registry
-manifest=$(docker manifest inspect "$image_name" 2>/dev/null || true)
-if [ -z "$manifest" ]; then
-    if [ "$MULTI_ARCH_ASSERT_IMG" = false ]; then
-        docker build -t "$image_name" -f Dockerfile.asserts . $DOCKER_BUILD_OPTIONS
-    else
+if [ "$MULTI_ARCH_ASSERT_IMG" = false ]; then
+    docker build -t "$image_name" -f Dockerfile.asserts . $DOCKER_BUILD_OPTIONS
+else
+    # check if the image is in the container registry
+    manifest=$(docker manifest inspect "$image_name" 2>/dev/null || true)
+    if [ -z "$manifest" ]; then
         echo "the e2e test asserts container image is not available in the remote registry. locally building and pushing into remote registry"
         docker buildx build --push \
             --progress=plain \
             --platform ${PLATFORMS} \
             --file Dockerfile.asserts \
             --tag ${image_name} .
-    fi   
-else
-    echo "the e2e test asserts container image is available in the remote registry. pulling into local environment"
-    docker pull "$image_name"
-fi
+    else
+        echo "the e2e test asserts container image is available in the remote registry. pulling into local environment"
+        docker pull "$image_name"
+    fi
+fi   
+
 echo "$image_name" > "$timestamp_file"
