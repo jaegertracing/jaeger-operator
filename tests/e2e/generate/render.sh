8d5141f28d03eeb50762dd0e02d6c0503160db39
Israel Blancas <iblancasa@gmail.com>
2023-01-05 09:18:40 +0100

    Disable generate E2E test in OCP (#2146)
    * Disable generate E2E test in OCP
    
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>
    
    * Trigger Build
    
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>
    
    * Trigger Build
    
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>
    
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>

--- tests/e2e/generate/render.sh
+++ tests/e2e/generate/render.sh
@@ -2,18 +2,17 @@
 
 source $(dirname "$0")/../render-utils.sh
 
-is_secured="false"
-if [ $IS_OPENSHIFT = true ]; then
-    is_secured="true"
-fi
-
 ###############################################################################
 # TEST NAME: generate
 ###############################################################################
-start_test "generate"
-# JAEGER_VERSION environment variable is set before this script is called
-jaeger_name="my-jaeger"
+if [ $IS_OPENSHIFT = true ]; then
+    skip_test "generate" "This test was skipped until https://github.com/jaegertracing/jaeger-operator/issues/2145 is fixed"
+else
+    start_test "generate"
+    # JAEGER_VERSION environment variable is set before this script is called
+    jaeger_name="my-jaeger"
 
-$GOMPLATE -f ./jaeger-template.yaml.template -o ./jaeger-deployment.yaml
+    $GOMPLATE -f ./jaeger-template.yaml.template -o ./jaeger-deployment.yaml
 
-render_smoke_test "$jaeger_name" "$is_secured" "01"
+    render_smoke_test "$jaeger_name" "false" "01"
+fi
